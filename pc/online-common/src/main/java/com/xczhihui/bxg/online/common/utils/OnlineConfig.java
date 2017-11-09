284b8952a77ab91c9f9b16e526be8c4063c73dbd
yuruixin <yuruixin_china@163.com>
2017-11-09 11:31:10 +0800

    cc视频相关配置注释打开

--- pc/online-common/src/main/java/com/xczhihui/bxg/online/common/utils/OnlineConfig.java
+++ pc/online-common/src/main/java/com/xczhihui/bxg/online/common/utils/OnlineConfig.java
@@ -16,24 +16,24 @@ public class OnlineConfig {
 		}
     }
 	
-//	//OFFICE365用户ID
-//	public static final String OFFICE365_USER_ID = getValue("OFFICE365_USER_ID");
-//	//OFFICE365获取文件的地址
-//	public static final String OFFICE365_URL = getValue("OFFICE365_URL");
-//	//OFFICE365向量
-//	public static final String OFFICE365_XL = getValue("OFFICE365_XL");
-//	//OFFICE365密钥
-//	public static final String OFFICE365_KEY = getValue("OFFICE365_KEY");
-//	//CC视频USERID
-//	public static final String CC_USER_ID = getValue("CC_USER_ID");
-//	//CC直播USERID
-//	public static final String CC_LIVE_USER_ID = getValue("CC_LIVE_USER_ID");
-//	//CC视频API KEY
-//	public static final String CC_API_KEY = getValue("CC_API_KEY");
-//	//CC直播API KEY
-//	public static final String CC_LIVE_API_KEY = getValue("CC_LIVE_API_KEY");
-//	//CC播放器ID
-//	public static final String CC_PLAYER_ID = getValue("CC_PLAYER_ID");
+	//OFFICE365用户ID
+	public static final String OFFICE365_USER_ID = getValue("OFFICE365_USER_ID");
+	//OFFICE365获取文件的地址
+	public static final String OFFICE365_URL = getValue("OFFICE365_URL");
+	//OFFICE365向量
+	public static final String OFFICE365_XL = getValue("OFFICE365_XL");
+	//OFFICE365密钥
+	public static final String OFFICE365_KEY = getValue("OFFICE365_KEY");
+	//CC视频USERID
+	public static final String CC_USER_ID = getValue("CC_USER_ID");
+	//CC直播USERID
+	public static final String CC_LIVE_USER_ID = getValue("CC_LIVE_USER_ID");
+	//CC视频API KEY
+	public static final String CC_API_KEY = getValue("CC_API_KEY");
+	//CC直播API KEY
+	public static final String CC_LIVE_API_KEY = getValue("CC_LIVE_API_KEY");
+	//CC播放器ID
+	public static final String CC_PLAYER_ID = getValue("CC_PLAYER_ID");
 	//微信开发平台应用id
     public static final String APP_ID = getValue("WX_APP_ID");
     //商户id
