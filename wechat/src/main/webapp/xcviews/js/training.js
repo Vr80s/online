24e55b150ff83437dfc3806c6bbba16e7aa08153
haozhongpin <850180305@qq.com>
2017-11-10 11:31:47 +0800

    修改。支付底栏高度。限制手机号只能输入数字。增加手机号输入时的错误弹框

--- wechat/src/main/webapp/xcviews/js/training.js
+++ wechat/src/main/webapp/xcviews/js/training.js
@@ -23,8 +23,8 @@
     }
     
     requestService("/bxg/bunch/offLineClassItem",{id : getQueryString("id"),userId:localStorage.userId},
-			   function(data) {
-    	result = data.resultObject;
+               function(data) {
+        result = data.resultObject;
         /**
          * 这里判断是否需要密码确认和是否付费
          */
@@ -47,8 +47,8 @@
             /* 点击线下课程出现的弹框，测试提出bug，就给隐藏掉了。*/
 
               requestService("/bxg/common/getSystemTime",{course_id : 1},
-			   		function(data) {
-            	  var serverTime=data;
+                    function(data) {
+                  var serverTime=data;
                   var endTime = Date.parse(new Date(result.endTime));
                   if(endTime<serverTime){
                       $(".training_teacher_bto").show();
@@ -59,7 +59,7 @@
                       $(".footer_buyDiv").show();
                       $("#bmspan").html("<div class=\"training_teacher_bto_right_close\" id=\"bmbtn\">已报名222</div>");
                   }*/
-            	  
+                  
               },false);
             
         }else if(result.watchState == 2){  //goto 付费页面
@@ -75,49 +75,58 @@
          $("#gradeName").text(result.gradeName);
         $(".lname").html(result.name);
         $("#address").html(result.address);
-		$("#udescription").html(result.udescription);
+        $("#udescription").html(result.udescription);
         //
         $("#details").text(result.description);
         $("#date").html(formatDateTime(result.startTime)+"-"+formatDateTime(result.endTime).substr(5));
-	 },false);
+     },false);
    
 
     function showBaseInfoDiv(){
-    	requestService("/bxg/apply/get",{id:1},
-				   function(data) {
-    		if(data.success){
+        requestService("/bxg/apply/get",{id:1},
+                   function(data) {
+            if(data.success){
                 var result = data.resultObject;
                 $("#phone").val(result.mobile);
                 $("#realName").val(result.realName);
             }
-	    },false);
+        },false);
         $("#baseInfoDiv").show();
-	}
+    }
 
     function apply(){
-
+        var phone =$("#phone").val();
+        
+        
         if($("#phone").val()==''){
             alert("手机号不能为空!");
             return false;
-		}
-		if($("#realName").val()==''){
+        }
+        
+        if (!(/^1[34578]\d{9}$/.test(phone))) {
+            alert("请输入正确的手机号");
+            return false;
+        }
+        
+        
+        if($("#realName").val()==''){
             alert("姓名不能为空!");
             return false;
-		}
+        }
 
 
-		   requestService("/bxg/apply/updateBaseInfo",{phone:$("#phone").val(),realName:$("#realName").val()},
-				   function(data) {
-			 //显示基本报名信息
-		                if(data.success){
-		                    goPay();
-						}
-	        },false);
-		
+           requestService("/bxg/apply/updateBaseInfo",{phone:$("#phone").val(),realName:$("#realName").val()},
+                   function(data) {
+             //显示基本报名信息
+                        if(data.success){
+                            goPay();
+                        }
+            },false);
+        
 
-		
+        
 
-	}
+    }
 
     function  goPay() {
 
