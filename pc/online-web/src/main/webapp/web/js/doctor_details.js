1e008f4f0392c0aaceb02255554c86a70daa30b2
suixin <1987629290@qq.com>
2017-12-13 23:36:31 +0800

    详细信息修改

--- pc/online-web/src/main/webapp/web/js/doctor_details.js
+++ pc/online-web/src/main/webapp/web/js/doctor_details.js
@@ -18,27 +18,27 @@ $(function(){
 	 	id:id,
 	 }, function (data) {
        console.log(data);
-       //医师名字
-       $('.header_inf_right>span:first-child').text(data.resultObject.name);
-       //医师职位
-       $('.header_inf_right>span:nth-child(2)').text(data.resultObject.title);
-       //医师头像
-       $('.header_inf_left').attr('background-image','url('+data.resultObject.headPortrait+')');
-       //职位渲染
-       $('.zhiwu').html("<em></em>"+data.resultObject.title+"");
-     
-       //所在城市渲染
-       $('.dizhi').html("<em></em>"+data.resultObject.city+"");
-       //主治症状
-       for(var i = 0; i < data.resultObject.fields.length; i++){
-       	var span = $('<span></span>');
-       	span.text(data.resultObject.fields[i].name);
-       	$('.doctor_inf2 > p').append(span);
-       }
+//     //医师名字
+//     $('.header_inf_right>span:first-child').text(data.resultObject.name);
+//     //医师职位
+//     $('.header_inf_right>span:nth-child(2)').text(data.resultObject.title);
+//     //医师头像
+//     $('.header_inf_left').attr('background-image','url('+data.resultObject.headPortrait+')');
+//     //职位渲染
+//     $('.zhiwu').html("<em></em>"+data.resultObject.title+"");
+//   
+//     //所在城市渲染
+//     $('.dizhi').html("<em></em>"+data.resultObject.city+"");
+//     //主治症状
+//     for(var i = 0; i < data.resultObject.fields.length; i++){
+//     	var span = $('<span></span>');
+//     	span.text(data.resultObject.fields[i].name);
+//     	$('.doctor_inf2 > p').append(span);
+//     }
        //医师描述
        $('.doctor_int>p').text(data.resultObject.description)
-   		
-   		
+// 		
+// 		
        if(data.resultObject.medicalHospital){
        	
        	  $('.yiguan').html("<em></em>"+data.resultObject.medicalHospital.name+"");
@@ -56,19 +56,10 @@ $(function(){
        //医馆地址
        $('.hospital_inf>p:nth-child(4)>span').text(data.resultObject.medicalHospital.province+data.resultObject.medicalHospital.city)
        }
-       
-       //渲染医馆简介
-//     var con = data.resultObject.description;
-//     $('.hospital_detail_inf>p').text(con);
-//     //联系方式 
-//     var tel = data.resultObject.tel;
-//     $('.hospital_detail_inf_bottom_tel').text('联系电话：'+tel)
-//     //地址渲染
-//     var sheng = data.resultObject.province;
-//     var shi =  data.resultObject.city;
-//     $('.sheng').text(sheng);
-//     $('.shi').text(shi);
-      
+       //顶部渲染
+        $('#doctor_detail_header').html(template('doctor_detail_topTpl',data.resultObject));
+        //所在医馆信息渲染
+//     $('#doc_hospital').html(template('doc_hospitalTpl',data.resultObject));
     });
     
     
