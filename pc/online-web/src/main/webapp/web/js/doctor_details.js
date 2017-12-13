9ae9c959dad4cadf91f66ffa4d5d28ada6e543f5
suixin <1987629290@qq.com>
2017-12-13 14:54:17 +0800

    医师详情接口调用

--- pc/online-web/src/main/webapp/web/js/doctor_details.js
+++ pc/online-web/src/main/webapp/web/js/doctor_details.js
@@ -19,27 +19,32 @@ $(function(){
 	 }, function (data) {
        console.log(data);
        //医师头像
-       var pic = data.resultObject.headPortrait;
-       $('.header_inf_left').attr('background-image','url('+pic+')')
+       $('.header_inf_left').attr('background-image','url('+data.resultObject.headPortrait+')')
        //职位渲染
-       var zhiwu = data.resultObject.title;
-       $('.zhiwu').html("<em></em>"+zhiwu+"");
+       $('.zhiwu').html("<em></em>"+data.resultObject.title+"");
        //所在医院渲染
-       var yiguan = data.resultObject.hospitalName;
-       $('.yiguan').html("<em></em>"+yiguan+"");
+       $('.yiguan').html("<em></em>"+data.resultObject.medicalHospital.name+"");
        //所在城市渲染
-       var city = data.resultObject.city;
-       $('.dizhi').html("<em></em>"+city+"");
+       $('.dizhi').html("<em></em>"+data.resultObject.city+"");
+       //主治症状
+       for(var i = 0; i < data.resultObject.fields.length; i++){
+       	var span = $('<span></span>');
+       	span.text(data.resultObject.fields[i].name);
+       	$('.doctor_inf2 > p').append(span);
+       }
        //医师描述
-       var dis = data.resultObject.description;
-       $('.doctor_int>p').text(dis)
+       $('.doctor_int>p').text(data.resultObject.description)
+       
        //坐诊医馆信息
-       $('.hospital_inf>p:first-child').text(yiguan);
-       //预约电话
-       $('.hospital_inf>p:nth-child(2)>span').text(yiguan);
+       //名称
+       $('.hospital_inf>p:first-child').text(data.resultObject.medicalHospital.name);
+       //电话
+       $('.hospital_inf>p:nth-child(2)>span').text(data.resultObject.medicalHospital.tel);
        //坐诊时间
        var worktime = data.resultObject.workTime;
        $('.hospital_inf>p:nth-child(3)>span').text(worktime);
+       //医馆地址
+       $('.hospital_inf>p:nth-child(4)>span').text(data.resultObject.medicalHospital.province+data.resultObject.medicalHospital.city)
        //渲染医馆简介
 //     var con = data.resultObject.description;
 //     $('.hospital_detail_inf>p').text(con);
@@ -53,5 +58,21 @@ $(function(){
 //     $('.shi').text(shi);
       
     });
+    
+    
+    
+//  RequestService("/medical/doctor/getDoctorById", "GET", {
+//	 	id:id
+//	 }, function (data) {
+//	        if(!data.resultObject){
+//	        	//没有数据处理
+//	           alert("没有数据/搜索失败")
+//	        }else{
+//	        	//获取到数据渲染
+//	        	//创建一个盒子
+//	        	console.log(data);
+//	           $('#doctor_detail_header').html(template('doctor_detail_topTpl',{doctor:data.resultObject}));
+//	        }
+//	    });
 	
 })
