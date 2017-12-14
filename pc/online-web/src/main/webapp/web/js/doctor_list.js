2cd0b09144d0d7169970ff6a6a247f4850d14ae2
suixin <1987629290@qq.com>
2017-12-14 11:16:05 +0800

    修改医师领域跳转、名医推荐部分

--- pc/online-web/src/main/webapp/web/js/doctor_list.js
+++ pc/online-web/src/main/webapp/web/js/doctor_list.js
@@ -9,25 +9,25 @@ $(function(){
     }
     return null;
 	}
-	var type = getQueryString('type');
 
 
 	//初始化请求信息
 	window.current = 1;
 	window.size = 8;
 	window.name = "";
-	window.type =getQueryString(type)?getQueryString(type):"";
-	console.log(type)
-	getHostipalList(current,size,name,type);
+	window.type =getQueryString('type')?getQueryString('type'):"";
+	window.field=getQueryString('field')?getQueryString('field'):"";
+	getHostipalList(current,size,name,type,field);
 	
 	
 	//渲染医师列表方法
-	function getHostipalList(current,size,name,type){
+	function getHostipalList(current,size,name,type,field){
 	    RequestService("/medical/doctor/getDoctors","GET",{ 
 	    	current:current,
 	    	size:size,
 	    	name:name,
-	    	type:type
+	    	type:type,
+	    	field:field
 	    },function(data){
 	        if(data.resultObject.records.length == 0){
 	        	//没有数据处理 
@@ -35,7 +35,7 @@ $(function(){
 	        }else{
 	        	//获取到数据渲染
 	        	//创建一个盒子
-	           $('#doctor_list').append(template('doctorListTpl',{doctor:data.resultObject.records}));
+	           $('#doctor_list').append(template('doctorListTpl',data.resultObject));
 	        }
 	    });
 	}
@@ -45,7 +45,7 @@ $(function(){
 //		if(e.keyCode==13){
 		  var name =$('.doctor_search_ipt > input').val();
 		  console.log(name)
-		  getHostipalList(current,size,name,type);
+		  getHostipalList(current,size,name,type,field);
 //		}
 		});
 		
@@ -54,15 +54,16 @@ $(function(){
 	    $('.more_doctor').click(function(){
 	    	current +=1;
 	    	console.log(current)
-	    	getHostipalList(current,size,name,type);
+	    	getHostipalList(current,size,name,type,field);
 	    })
 	    
 	    
 	    //名师推荐部分
 	     RequestService("/medical/doctor/getRecDoctors","GET",null,function(data){
 	       
-	        	console.log(data);
-	           $('#doc_rec').html(template('doc_recTpl',{doc:data.resultObject}));
+	        console.log(data);
+	        $('#doc_rec').html(template('doc_recTpl',{doc:data.resultObject}));
+	        
 	      });
 //	    });
 	    
