846a6db17ec0d5ca4dd213453ccafdb8c0be92c7
yuruixin <yuruixin_china@163.com>
2017-12-16 00:10:45 +0800

    修改banner点击跳转不正确问题

--- pc/online-web/src/main/webapp/web/js/doctor.js
+++ pc/online-web/src/main/webapp/web/js/doctor.js
@@ -45,7 +45,7 @@ $(function () {
     var articleBanner = '{{each articleBanner}}' +
             '{{if $index==0}}'+
         '<li style="z-index: 2">' +
-        '<a href="/web/html/forumDetail.html?Id={{$value.id}}" target="_blank" style="background:url({{$value.imgPath}})no-repeat top center">' +
+        '<a href="{{$value.imgHref}}" target="_blank" style="background:url({{$value.imgPath}})no-repeat top center">' +
         '<div class="banner-info">' +
         '<span class="banner-type">{{$value.name}}</span>' +
         '<span class="banner-title">{{$value.title}}</span>' +
@@ -55,7 +55,7 @@ $(function () {
         '</li>' +
         '{{else}}'+
         '<li>' +
-        '<a href="/web/html/forumDetail.html?Id={{$value.id}}" target="_blank" style="background:url({{$value.imgPath}})no-repeat top center">' +
+        '<a href="{{$value.imgHref}}" target="_blank" style="background:url({{$value.imgPath}})no-repeat top center">' +
         '<div class="banner-info">' +
         '<span class="banner-type">{{$value.name}}</span>' +
         '<span class="banner-title">{{$value.title}}</span>' +
