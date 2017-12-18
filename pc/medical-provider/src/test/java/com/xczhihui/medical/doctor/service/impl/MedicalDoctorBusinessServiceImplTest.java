60f728d277b4abfec6a9f6d2df9bc1ca2906a450
yuruixin <yuruixin_china@163.com>
2017-12-18 15:13:38 +0800

    优化医馆查询关联效率

---
+++ pc/medical-provider/src/test/java/com/xczhihui/medical/doctor/service/impl/MedicalDoctorBusinessServiceImplTest.java
@@ -0,0 +1,35 @@
+package com.xczhihui.medical.doctor.service.impl;
+
+import com.xczhihui.medical.doctor.model.MedicalDoctor;
+import com.xczhihui.medical.doctor.service.IMedicalDoctorBusinessService;
+import com.xczhihui.medical.hospital.model.MedicalHospital;
+import com.xczhihui.medical.hospital.service.IMedicalHospitalBusinessService;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import test.BaseJunit4Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * ClassName: UserCoin.java <br>
+ * Description:用户-代币余额表 <br>
+ * Create by: name：yuxin <br>email: yuruixin@ixincheng.com <br>
+ * Create Time: 上午 11:37 2017/12/18 0018<br>
+ */
+public class MedicalDoctorBusinessServiceImplTest  extends BaseJunit4Test {
+
+    @Autowired //自动注入
+    private IMedicalDoctorBusinessService iMedicalDoctorBusinessService;
+
+    @Test
+//    @Transactional   //标明此方法需使用事务
+//    @Rollback(false)  //标明使用完此方法后事务不回滚,true时为回滚
+    public void test1(){
+        System.out.println("测试iMedicalHospitalBusinessService");
+        MedicalDoctor medicalDoctor = iMedicalDoctorBusinessService.selectDoctorById("3476f961251b4094b020b560b8fddf48");
+        System.out.println(medicalDoctor.toString());
+    }
+
+}
--- pc/medical-provider/src/test/java/com/xczhihui/medical/hospital/service/impl/MedicalHospitalTest.java
+++ pc/medical-provider/src/test/java/com/xczhihui/medical/hospital/service/impl/MedicalHospitalTest.java
@@ -3,9 +3,12 @@ package com.xczhihui.medical.hospital.service.impl;
 import com.baomidou.mybatisplus.plugins.Page;
 import com.xczhihui.medical.hospital.model.MedicalHospital;
 import com.xczhihui.medical.hospital.service.IMedicalHospitalBusinessService;
+import org.apache.ibatis.reflection.SystemMetaObject;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import test.BaseJunit4Test;
+
+import java.util.List;
 
 /**
  * ClassName: UserCoin.java <br>
@@ -21,26 +24,89 @@ public class MedicalHospitalTest  extends BaseJunit4Test {
     @Autowired //自动注入
     private IMedicalHospitalBusinessService iMedicalHospitalBusinessService;
 
-//    @Test
+    @Test
 //    @Transactional   //标明此方法需使用事务
 //    @Rollback(false)  //标明使用完此方法后事务不回滚,true时为回滚
-//    public void test1(){
-//        System.out.println("测试Spring整合Junit4进行单元测试");
-//        List<User> users = iUserService.selectList(null);
-//        for (User user : users) {
-//            System.out.println(user.getName());
+    public void test1(){
+        System.out.println("测试iMedicalHospitalService");
+        List<MedicalHospital> medicalHospitals = iMedicalHospitalBusinessService.selectRecHospital();
+        for (MedicalHospital medicalHospital : medicalHospitals) {
+            System.out.println(medicalHospital.getName());
+            System.out.println(medicalHospital.getId());
+        }
+    }
+
+    @Test
+    public void test11(){
+        System.out.println("测试遍历查询");
+//        long startTime = System.currentTimeMillis(); // 获取开始时间
+//        for (int i = 0; i < 10; i++) {
+//            MedicalHospital medicalHospitals = iMedicalHospitalBusinessService.selectHospitalById("88ebc1ee4e264ce7a81c89abd06de12e");
+////            System.out.println(medicalHospitals.getName());
 //        }
-//    }
+//        long endTime = System.currentTimeMillis(); // 获取结束时间
+//        long consuming = endTime - startTime;
+        int a = 10;
+        int b = 5000;
+        long byIdTime = 0;
+        for (int i = 0; i < a; i++) {
+            byIdTime += getByIdTime(b,iMedicalHospitalBusinessService);
+        }
+        System.out.println("测试遍历查询----平均用时用时"+byIdTime+"ms,平均用时"+byIdTime/(a*b)+"ms");
+
+    }
+
+    public long getByIdTime(int count,IMedicalHospitalBusinessService iMedicalHospitalBusinessService){
+        long startTime = System.currentTimeMillis(); // 获取开始时间
+        for (int i = 0; i < count; i++) {
+            MedicalHospital medicalHospitals = iMedicalHospitalBusinessService.selectHospitalById("85d9ca35f56e431d9de4570985972401");
+//            System.out.println(medicalHospitals.getName());
+        }
+        long endTime = System.currentTimeMillis(); // 获取结束时间
+        long consuming = endTime - startTime;
+        System.out.println(consuming);
+        return consuming;
+    }
+
+
+    @Test
+    public void test111(){
+        System.out.println("测试一次查询");
+        int a = 10;
+        int b = 5000;
+        long byIdTime = 0;
+        for (int i = 0; i < a; i++) {
+            byIdTime += getByIdTestTime(b,iMedicalHospitalBusinessService);
+        }
+        System.out.println("测试一次查询----平均用时用时"+byIdTime+"ms,平均用时"+byIdTime/(a*b)+"ms");
+    }
+
+
+    public long getByIdTestTime(int count,IMedicalHospitalBusinessService iMedicalHospitalBusinessService){
+        long startTime = System.currentTimeMillis(); // 获取开始时间
+        for (int i = 0; i < count; i++) {
+            MedicalHospital medicalHospitals = iMedicalHospitalBusinessService.selectHospitalById("88ebc1ee4e264ce7a81c89abd06de12e");
+//            System.out.println(medicalHospitals.getName());
+        }
+        long endTime = System.currentTimeMillis(); // 获取结束时间
+        long consuming = endTime - startTime;
+        System.out.println(consuming);
+        return consuming;
+    }
 
     @Test
 //    @Transactional   //标明此方法需使用事务
 //    @Rollback(false)  //标明使用完此方法后事务不回滚,true时为回滚
     public void test2(){
-//        System.out.println("测试iMedicalHospitalService");
-//        List<MedicalHospital> medicalHospitals = iMedicalHospitalBusinessService.selectList(null);
-//        for (MedicalHospital medicalHospital : medicalHospitals) {
-//            System.out.println(medicalHospital.toString());
-//        }
+        System.out.println("测试iMedicalHospitalService");
+        Page<MedicalHospital> page = new Page<>();
+        page.setSize(100);
+        page.setCurrent(0);
+        Page<MedicalHospital> page1 = iMedicalHospitalBusinessService.selectHospitalPage(page, null, null);
+        List<MedicalHospital> medicalHospitals = page1.getRecords();
+        for (MedicalHospital medicalHospital : medicalHospitals) {
+            System.out.println(medicalHospital.toString());
+        }
     }
 
 
@@ -48,13 +114,13 @@ public class MedicalHospitalTest  extends BaseJunit4Test {
 //    @Transactional   //标明此方法需使用事务
 //    @Rollback(false)  //标明使用完此方法后事务不回滚,true时为回滚
     public void test3(){
-        System.out.println("测试iMedicalHospitalService");
-        Page<MedicalHospital> page = new Page<>();
-        page.setCurrent(1);
-        page.setSize(5);
-        Page<MedicalHospital> medicalHospitalPage = iMedicalHospitalBusinessService.selectHospitalPage(page,"www", "");
-        for (MedicalHospital medicalHospital : medicalHospitalPage.getRecords()) {
-            System.out.println(medicalHospital.toString());
-        }
+//        System.out.println("测试iMedicalHospitalService");
+//        Page<MedicalHospital> page = new Page<>();
+//        page.setCurrent(1);
+//        page.setSize(5);
+//        Page<MedicalHospital> medicalHospitalPage = iMedicalHospitalBusinessService.selectHospitalPage(page,"www", "");
+//        for (MedicalHospital medicalHospital : medicalHospitalPage.getRecords()) {
+//            System.out.println(medicalHospital.toString());
+//        }
     }
 }
