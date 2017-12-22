512e3ede4791e4550e1250923f68f28a3ed2bcff
suixin <1987629290@qq.com>
2017-12-22 21:53:40 +0800

    专栏详情页面渲染

---
+++ pc/online-web/src/main/webapp/web/js/zhuanlan_detail.js
@@ -0,0 +1,41 @@
+$(function(){
+	
+	//获取url中参数值的方法
+	function getQueryString(name) {
+    var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) {
+        return (r[2]); 
+    }
+    return null;
+	}
+	
+	var articleId =  getQueryString('id');
+	
+	
+	    // 报道详情页面数据请求
+	    RequestService("/medical/doctor/getSpecialColumnDetailsById","GET",{
+	    	articleId:articleId
+	    },function(data){
+	        if(data.success==false ||data.resultObject==null|| data.resultObject.length == 0){
+	           $('.teacher_books').addClass('hide')
+	        }else{
+	        	//获取到数据渲染
+	        	console.log(data)
+	           $('#report_box').html(template('report_detailTpl',data.resultObject));
+	           
+	        
+
+				if(data.resultObject.medicalDoctors.length == 0){
+					 $('#report_doctor_list').addClass('hide');
+				}else{
+					$('#report_doctor_list').html(template('report_docTpl',{inf:data.resultObject.medicalDoctors}));
+				}
+	          
+	        }
+	    });
+	    
+	    
+	    
+	    
+})
