258c2c46592048657630f5093170ce9a30674dfb
suixin <1987629290@qq.com>
2018-01-04 10:13:47 +0800

    修改bug 1180

--- pc/online-web/src/main/webapp/web/js/report.js
+++ pc/online-web/src/main/webapp/web/js/report.js
@@ -5,6 +5,11 @@
  * Created by admin on 2017/1/3.
  */
 $(function(){
+	
+	$('.forum').css('color','#000');
+	$('.path .doctor').addClass('select');
+	
+	
     $(".path a").each(function(){
         if($(this).text()=="博学社"){
             $(this).addClass("select").siblings().removeClass("select");
@@ -53,7 +58,7 @@ $(function(){
             '<img src="/web/images/studentCount.png" />'+
 //          '<i class="iconfont icon-biaoqian"></i>{{#tagGroup(items.tag,items.tagId)}}'+
             '<span>{{author}}&nbsp;&nbsp;&nbsp;{{dataSub(createTime)}}</span>'+
-            '<a href="/web/html/practitioneNews.html" style="color: #188EEE;margin-left:20px">媒体报道</a>'+
+            '<a href="/web/html/practitioneNews.html" style="color: #188EEE;margin-left:20px">名医报道</a>'+
 //          '<span>阅读({{items.browse_sum}})</span>'+
 //          '<span>点赞(<span class="articleHitCoun">{{items.praise_sum}}</span>)</span>'+
 //          '<span>评论(<span class="articleCommCoun">{{items.comment_sum}}</span>)</span>'+
