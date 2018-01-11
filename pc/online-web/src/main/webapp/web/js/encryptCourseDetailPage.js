6fbf4e5422b6fe650750f1d8fdafbe321df2967d
yuruixin <yuruixin_china@163.com>
2018-01-11 20:58:35 +0800

    课程详情中的评论添加星级

--- pc/online-web/src/main/webapp/web/js/encryptCourseDetailPage.js
+++ pc/online-web/src/main/webapp/web/js/encryptCourseDetailPage.js
@@ -146,7 +146,9 @@ window.onload=function(){
 		'<span class="repuName" title="{{$value.userName}}">{{$value.userName}}</span>' +
 		'</div>' +
 		'<div class="good-detail-info">' +
-
+        '<div class="starts">' +
+        '{{#stuEvluatStars($value.starLevel)}}' +
+        '</div>' +
 		'<div class="reputationContent">{{$value.content}}</div>' +
 		'<div class="repuationRelatInfo clearfix">' +
 		'<div class="repuTime">时间：{{dataSub($value.createTime)}}</div>' +
