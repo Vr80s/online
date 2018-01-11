8a97a8ad68b9412a42d04603a86d76e97976aa51
wangxingchuan <wangxingchuan@ixincheng.com>
2018-01-11 17:29:22 +0800

    医师下拉小箭头设置

--- pc/online-web/src/main/webapp/web/js/anchors_resources.js
+++ pc/online-web/src/main/webapp/web/js/anchors_resources.js
@@ -8,10 +8,14 @@ $(function(){
 			})
 			$(".select_list .select-ud").bind('click',function(event){
             	event.stopPropagation();			
-				$(".select_list .littleBox").stop().slideToggle()
+				$(".select_list .littleBox").stop().slideToggle();
+
 			})
 			$(".setTop").click(function(){
 				$(".select_list .littleBox").slideUp()
+				$(".select-ud").removeAttr("id")
+				$(".select_list .arrow_jt").removeClass("glyphicon-triangle-bottom")
+				$(".select_list .arrow_jt").addClass("glyphicon-triangle-left")		
 			})
 //			对课程目录下小的下拉进行操作		
 			$(".select_list .littleBox p").bind('click',function(event){
@@ -21,4 +25,19 @@ $(function(){
 				$(".wrap_box .little_box").hide().eq($(this).index()).show();
 				$(".select_box").hide()
 			})
+//		下拉小箭头设置	
+		$(".select-ud").click(function(){
+			if($(this).attr("id")=="open_list"){
+				$(this).removeAttr("id")
+				$(".select_list .arrow_jt").addClass("glyphicon-triangle-left")
+				$(".select_list .arrow_jt").removeClass("glyphicon-triangle-bottom")
+			}
+			else{
+				$(this).attr("id","open_list")
+				$(".select_list .arrow_jt").addClass("glyphicon-triangle-bottom")
+				$(".select_list .arrow_jt").removeClass("glyphicon-triangle-left")
+			}
+		})
+
+
 })
