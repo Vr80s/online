773c8d95fc55305c5bea6276c3f455f9d9512838
yuruixin <yuruixin_china@163.com>
2018-01-15 17:01:24 +0800

    医馆入驻相关代码

---
+++ pc/medical-provider/src/main/java/com/xczhihui/medical/hospital/mapper/MedicalHospitalAuthenticationApplyMapper.java
@@ -0,0 +1,17 @@
+package com.xczhihui.medical.hospital.mapper;
+
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.xczhihui.medical.hospital.model.MedicalHospitalAuthenticationApply;
+
+/**
+ * <p>
+  *  Mapper 接口
+ * </p>
+ *
+ * @author yuxin
+ * @since 2018-01-15
+ */
+public interface MedicalHospitalAuthenticationApplyMapper extends BaseMapper<MedicalHospitalAuthenticationApply> {
+
+}
