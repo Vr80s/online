d2c2e436e3b55012e0adb980b25a962fd8e69a4d
zhuwenbao <546507686@qq.com>
2018-01-16 15:45:04 +0800

    医师入驻申请api

---
+++ pc/medical-api/src/main/java/com/xczhihui/medical/doctor/service/IMedicalDoctorApplyService.java
@@ -0,0 +1,17 @@
+package com.xczhihui.medical.doctor.service;
+
+
+import com.xczhihui.medical.doctor.model.MedicalDoctorApply;
+
+/**
+ *  医师入驻申请服务类
+ *  @author zhuwenbao
+ */
+public interface IMedicalDoctorApplyService {
+
+    /**
+     * 添加医师入驻申请信息
+     * @param target 医师申请入驻申请信息
+     */
+    void add(MedicalDoctorApply target);
+}
