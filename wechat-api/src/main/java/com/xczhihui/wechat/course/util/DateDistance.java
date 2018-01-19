b8b211fd6b747258fe27a15e925917c7d6210674
yangxuan <453099975@qq.com>
2018-01-19 17:12:12 +0800

    增加时间工具类

---
+++ wechat-api/src/main/java/com/xczhihui/wechat/course/util/DateDistance.java
@@ -0,0 +1,188 @@
+package com.xczhihui.wechat.course.util;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 时间相距
+ * ClassName: DateDistance.java <br>
+ * Description: <br>
+ * Create by: name：yangxuan <br>email: 15936216273@163.com <br>
+ * Create Time: 2017年8月14日<br>
+ */
+public class DateDistance {
+
+	/**
+	 * 两个时间之间相差距离多少天
+	 * 
+	 * @param one
+	 *            时间参数 1：
+	 * @param two
+	 *            时间参数 2：
+	 * @return 相差天数
+	 */
+	public static long getDistanceDays(String str1, String str2)
+			throws Exception {
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+		Date one;
+		Date two;
+		long days = 0;
+		try {
+			one = df.parse(str1);
+			two = df.parse(str2);
+			long time1 = one.getTime();
+			long time2 = two.getTime();
+			long diff;
+			if (time1 < time2) {
+				diff = time2 - time1;
+			} else {
+				diff = time1 - time2;
+			}
+			days = diff / (1000 * 60 * 60 * 24);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return days;
+	}
+
+	/**
+	 * 两个时间相差距离多少天多少小时多少分多少秒
+	 * 
+	 * @param str1
+	 *            时间参数 1 格式：1990-01-01 12:00:00
+	 * @param str2
+	 *            时间参数 2 格式：2009-01-01 12:00:00
+	 * @return long[] 返回值为：{天, 时, 分, 秒}
+	 */
+	public static long[] getDistanceTimes(String str1, String str2) {
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Date one;
+		Date two;
+		long day = 0;
+		long hour = 0;
+		long min = 0;
+		long sec = 0;
+		try {
+			one = df.parse(str1);
+			two = df.parse(str2);
+			long time1 = one.getTime();
+			long time2 = two.getTime();
+			long diff;
+			if (time1 < time2) {
+				diff = time2 - time1;
+			} else {
+				diff = time1 - time2;
+			}
+			day = diff / (24 * 60 * 60 * 1000);
+			hour = (diff / (60 * 60 * 1000) - day * 24);
+			min = ((diff / (60 * 1000)) - day * 24 * 60 - hour * 60);
+			sec = (diff / 1000 - day * 24 * 60 * 60 - hour * 60 * 60 - min * 60);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		long[] times = { day, hour, min, sec };
+		return times;
+	}
+
+	/**
+	 * 两个时间相差距离多少天多少小时多少分多少秒
+	 * 
+	 * @param str1
+	 *            时间参数 1 格式：1990-01-01 12:00:00
+	 * @param str2
+	 *            时间参数 2 格式：2009-01-01 12:00:00
+	 * @return String 返回值为：xx天xx小时xx分xx秒
+	 */
+	public static String getDistanceTime(String str1, String str2) {
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Date one;
+		Date two;
+		long day = 0;
+		long hour = 0;
+		long min = 0;
+		long sec = 0;
+		try {
+			one = df.parse(str1);
+			two = df.parse(str2);
+			long time1 = one.getTime();
+			long time2 = two.getTime();
+			long diff;
+			if (time1 < time2) {
+				diff = time2 - time1;
+			} else {
+				diff = time1 - time2;
+			}
+			day = diff / (24 * 60 * 60 * 1000);
+			hour = (diff / (60 * 60 * 1000) - day * 24);
+			min = ((diff / (60 * 1000)) - day * 24 * 60 - hour * 60);
+			sec = (diff / 1000 - day * 24 * 60 * 60 - hour * 60 * 60 - min * 60);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		if(day == 0){
+			if(hour == 0){
+				if(min == 0){
+					return "刚刚";
+				}else{
+					return min + "分前";
+				}
+			}else{
+				return hour + "小时" + min + "分前";
+			}
+		}else{
+			return day + "天" + hour + "小时" + min + "分前";
+		}
+	}
+	
+	/**
+	 * 新的原型设计学习中心的多少天多少天前
+	 * return day + "天" + hour + "小时" + min + "分前";
+	 * 
+	 * @param str1
+	 *            时间参数 1 格式：1990-01-01 12:00:00
+	 * @param str2
+	 *            时间参数 2 格式：2009-01-01 12:00:00
+	 * @return String 返回值为：xx天xx小时xx分xx秒
+	 */
+	public static String getNewDistanceTime(String str1, String str2) {
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Date one;
+		Date two;
+		long day = 0;
+		long hour = 0;
+		long min = 0;
+		long sec = 0;
+		try {
+			one = df.parse(str1);
+			two = df.parse(str2);
+			long time1 = one.getTime();
+			long time2 = two.getTime();
+			long diff;
+			if (time1 < time2) {
+				diff = time2 - time1;
+			} else {
+				diff = time1 - time2;
+			}
+			day = diff / (24 * 60 * 60 * 1000);
+			hour = (diff / (60 * 60 * 1000) - day * 24);
+			min = ((diff / (60 * 1000)) - day * 24 * 60 - hour * 60);
+			sec = (diff / 1000 - day * 24 * 60 * 60 - hour * 60 * 60 - min * 60);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		if(day == 0){
+			if(hour == 0){
+				return min + "分钟前";
+			}else{
+				return hour + "小时前";
+			}
+		}else if(day>0 && day<=7){
+			return day + "天前";
+		}else{
+			return "很久很久以前";
+		}
+	}
+	
+}
