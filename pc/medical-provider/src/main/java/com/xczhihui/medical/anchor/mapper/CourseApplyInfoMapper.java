cefd3f26c272c781b6e945c5b00738de9d268cad
yuruixin <yuruixin_china@163.com>
2018-01-19 10:36:02 +0800

    添加前台主播课程相关基础代码

---
+++ pc/medical-provider/src/main/java/com/xczhihui/medical/anchor/mapper/CourseApplyInfoMapper.java
@@ -0,0 +1,16 @@
+package com.xczhihui.medical.anchor.mapper;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.xczhihui.medical.anchor.model.CourseApplyInfo;
+
+/**
+ * <p>
+  *  Mapper 接口
+ * </p>
+ *
+ * @author yuxin
+ * @since 2018-01-19
+ */
+public interface CourseApplyInfoMapper extends BaseMapper<CourseApplyInfo> {
+
+}
---
+++ pc/medical-provider/src/main/java/com/xczhihui/medical/anchor/service/ICourseApplyInfoService.java
@@ -0,0 +1,16 @@
+package com.xczhihui.medical.anchor.service;
+
+import com.baomidou.mybatisplus.service.IService;
+import com.xczhihui.medical.anchor.model.CourseApplyInfo;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author yuxin
+ * @since 2018-01-19
+ */
+public interface ICourseApplyInfoService extends IService<CourseApplyInfo> {
+	
+}
