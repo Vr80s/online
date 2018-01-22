112a9642324ba8007efad8619c228efa16e9125d
yuruixin <yuruixin_china@163.com>
2018-01-22 14:03:34 +0800

    全局处理接口参数异常

--- wechat/src/main/java/com/xczh/consumer/market/utils/GlobalExceptionHandlerAdvice.java
+++ wechat/src/main/java/com/xczh/consumer/market/utils/GlobalExceptionHandlerAdvice.java
@@ -5,6 +5,8 @@ import java.io.StringWriter;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,7 +20,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class GlobalExceptionHandlerAdvice {
 
     private static final  String BIZ_RUNTIME_EXCEPTION_MESSAGE="服务器错误,请联系管理员";
-
+    private static final  String BIZ_Parameter_EXCEPTION_MESSAGE="参数有误";
+    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandlerAdvice.class);
     @ExceptionHandler(Exception.class)
     @ResponseBody
     public ResponseObject handleException(Exception ex) {
@@ -27,9 +30,11 @@ public class GlobalExceptionHandlerAdvice {
         ex.printStackTrace(pw);
         pw.flush();
         sw.flush();
-        System.out.println("运行时异常.message:"+ex.getMessage());
-        System.out.println("运行时异常.栈信息:"+sw.toString());
-
+        if(ex instanceof MissingServletRequestParameterException){
+            return  ResponseObject.newErrorResponseObject(BIZ_Parameter_EXCEPTION_MESSAGE);
+        }
+        LOGGER.error("运行时异常.message:"+ex.getMessage());
+        LOGGER.error("运行时异常.栈信息:"+sw.toString());
         return  ResponseObject.newErrorResponseObject(ex.getMessage()!=null?!isContainChinese(ex.getMessage().substring(0,1))?BIZ_RUNTIME_EXCEPTION_MESSAGE:ex.getMessage()==null?BIZ_RUNTIME_EXCEPTION_MESSAGE:ex.getMessage():BIZ_RUNTIME_EXCEPTION_MESSAGE);
     }
     public static boolean isContainChinese(String str) {
