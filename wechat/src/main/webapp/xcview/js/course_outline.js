5dc23ad4e3b5d34d701eedd2ce418338be52751b
haozhongpin <850180305@qq.com>
2018-01-24 09:16:41 +0800

    学堂--课程、大纲

---
+++ wechat/src/main/webapp/xcview/js/course_outline.js
@@ -0,0 +1,32 @@
+$(function(){
+
+
+//搜索历史开始
+
+requestService("/xczh/course/details",{courseId:748}, 
+    function(data) {
+		
+		if(data.success==true){
+		
+//		标题
+		$(".outline_main").html(template('outline_main',data.resultObject)) 	    	
+			
+//			大纲
+		$(".outline_list_ul").html(template('outline_list_ul',data.resultObject)) 	    	
+		}
+		
+},false) 
+
+
+
+//搜索历史结束
+
+
+
+
+
+
+
+
+})
+
