08dadca4593747e56072f11b8de47d7fcde0d656
yangxuan <453099975@qq.com>
2018-01-31 00:22:09 +0800

    增加新的提现、结算接口

--- wechat-provider/src/main/java/com/xczhihui/wechat/course/mapper/MyInfoMapper.java
+++ wechat-provider/src/main/java/com/xczhihui/wechat/course/mapper/MyInfoMapper.java
@@ -2,6 +2,7 @@ package com.xczhihui.wechat.course.mapper;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
 
 import com.baomidou.mybatisplus.mapper.BaseMapper;
 import com.xczhihui.wechat.course.model.OnlineUser;
@@ -16,6 +17,34 @@ import com.xczhihui.wechat.course.model.OnlineUser;
  */
 public interface MyInfoMapper extends BaseMapper<OnlineUser> {
 
-
+	/**
+	 * 
+	 * Description：查找主播的学员、课程数
+	 * @param userId
+	 * @return
+	 * @return List<BigDecimal>
+	 * @author name：yangxuan <br>email: 15936216273@163.com
+	 *
+	 */
 	List<BigDecimal> selectCollegeCourseXmbNumber(String userId);
+	
+	/**
+	 * Description：结算页面记录信息
+	 * @param userId
+	 * @return
+	 * @return List<Map<String,Object>>
+	 * @author name：yangxuan <br>email: 15936216273@163.com
+	 */
+	List<Map<String, Object>> selectSettlementList(String userId);
+	
+	/**
+	 * 
+	 * Description：提现页面记录信息
+	 * @param userId
+	 * @return
+	 * @return List<Map<String,Object>>
+	 * @author name：yangxuan <br>email: 15936216273@163.com
+	 *
+	 */
+	List<Map<String, Object>> selectWithdrawalList(String userId);
 }
