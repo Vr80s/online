399a9e4079021ced076924f7542b007a8f144b01
suixin <1987629290@qq.com>
2018-01-31 10:24:02 +0800

    修改医师医馆注册页面的bug

--- pc/online-web/src/main/webapp/web/js/hospitalAutdoc.js
+++ pc/online-web/src/main/webapp/web/js/hospitalAutdoc.js
@@ -17,5 +17,34 @@ $(function(){
 //			console.log(data)
 //		})
 	})
+	
+	
+	//医师预览功能
+$('.doc_Administration_bottom2 .preview').click(function(){
+	$('#mask').removeClass('hide');
+	$('#doc_Administration_bottom3').addClass('hide');
+	$('#doc_Administration_bottom4').removeClass('hide');
+})
+
+
+//医师编辑功能
+$('#doc_Administration_bottom2 .edit').click(function(){
+	$('#mask').removeClass('hide');
+	$('#doc_Administration_bottom4').addClass('hide');
+	$('#doc_Administration_bottom3').removeClass('hide');
+})
+
+
+//医师编辑关闭按钮
+$('#doc_Administration_bottom4 .close_doc_inf').click(function(){
+	$('#mask').addClass('hide');
+	$('#doc_Administration_bottom4').addClass('hide');
+})
+
+//医师预览关闭按钮
+$('#doc_Administration_bottom3 .close_doc_inf').click(function(){
+	$('#mask').addClass('hide');
+	$('#doc_Administration_bottom3').addClass('hide');
+})
 })
 	
