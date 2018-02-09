b02d2f47eee46175d3359f3c7de0300803a94fa0
yangxuan <453099975@qq.com>
2018-02-09 09:12:53 +0800

    修改检索内容去的显示

--- wechat/src/main/java/com/xczh/consumer/market/service/impl/OLAttachmentCenterServiceImpl.java
+++ wechat/src/main/java/com/xczh/consumer/market/service/impl/OLAttachmentCenterServiceImpl.java
@@ -3,6 +3,8 @@ package com.xczh.consumer.market.service.impl;
 
 import com.xczh.consumer.market.service.OLAttachmentCenterService;
 import com.xczh.consumer.market.utils.HttpUtil;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -12,19 +14,21 @@ import java.util.Map;
 public class OLAttachmentCenterServiceImpl implements OLAttachmentCenterService {
 
 
+	@Value("${rate}")
+	private String attachmentCenterPath;
+	
 	//<img src="http://attachment-center.ixincheng.com:38080/data/picture/online/2017/09/25/09/e9dfe660b8744af3b39ebd92165fc639.jpg">
-	private String attachmentCenterPath = "http://www.ixincheng.com:38080/";
+	//private String attachmentCenterPath = "http://172.26.108.181:38080/";
 	
 	@Override
 	public String upload(String createUserId, String projectName,
 			String fileName, String contentType, byte[] fileData,
 			String fileType, String ticket) {
-		// TODO Auto-generated method stub
+		
 		Map<String,String> parameters = new HashMap<String,String>();
 		parameters.put("createUserId",createUserId);
 		parameters.put("projectName",projectName);
 		parameters.put("fileType",fileType);
-		Map<String, String> cookies = new HashMap<String, String>();
 		
 		return HttpUtil.uploadFile(attachmentCenterPath+"/attachment/upload", parameters,
 				"attachment", fileName, contentType, fileData);
