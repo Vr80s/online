3a6520546d83708b209f152c90e88e228c3dc929
yuruixin <yuruixin_china@163.com>
2018-02-09 14:32:17 +0800

    no message

--- pc/online-web/src/main/java/com/xczhihui/bxg/online/web/controller/medical/AnchorController.java
+++ pc/online-web/src/main/java/com/xczhihui/bxg/online/web/controller/medical/AnchorController.java
@@ -1,18 +1,9 @@
 package com.xczhihui.bxg.online.web.controller.medical;
 
-import com.baomidou.mybatisplus.plugins.Page;
-import com.xczhihui.bxg.common.util.bean.ResponseObject;
-import com.xczhihui.bxg.common.web.util.UserLoginUtil;
-import com.xczhihui.bxg.online.common.domain.OnlineUser;
 import com.xczhihui.medical.anchor.service.IAssetService;
-import com.xczhihui.medical.anchor.service.IUserBankService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.Map;
 
 
 /**
@@ -23,8 +14,6 @@ import java.util.Map;
 @RequestMapping(value = "/anchor/info")
 public class AnchorController {
 
-    @Autowired
-    private IAssetService assetService;
 
 
 }
