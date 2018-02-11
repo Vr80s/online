454544c2e8776900b628ba5a8240b3c0ab9884ad
suixin <1987629290@qq.com>
2018-02-11 14:30:29 +0800

    医馆基础信息上传参数修改

--- pc/online-web/src/main/webapp/web/js/hospitalAuthentication.js
+++ pc/online-web/src/main/webapp/web/js/hospitalAuthentication.js
@@ -117,7 +117,7 @@ $(function(){
 		data.wechat =  $('#hos_Administration .hos_base_inf  .hos_weixin').val();
 		data.headPortrait = $('.hos_base_inf   .touxiang_pic img').attr('src');
         data.description = UE.getEditor('editor2').getContent();
-        data.detailAddress = $.trim($('#hos_Administration .hos_base_inf .doc_address textarea').val());
+        data.detailedAddress = $.trim($('#hos_Administration .hos_base_inf .doc_address textarea').val());
 		var hoslist = [];
 		for(var i = 0;i < $('#hos_pic img').length ; i++){
 			    hoslist.push($('#hos_pic img').eq(i).attr('src'));
