baeb35c672d03c7fa776a2790738f23e9011037b
wangxingchuan <wangxingchuan@ixincheng.com>
2018-02-24 14:30:16 +0800

    修改游客登陆字段判断

--- wechat/src/main/webapp/xcview/js/discovery.js
+++ wechat/src/main/webapp/xcview/js/discovery.js
@@ -1,4 +1,7 @@
 var status;
+//	判断是否为游客点击
+var user_cookie = cookie.get("_uc_t_");
+
 $(function(){
     //获取认证状态
     requestService("/xczh/medical/applyStatus",{
@@ -12,11 +15,14 @@ $(function(){
     });
 });
 
+	
 //点击我要当主播
 function myAnchor() {
     localStorage.setItem("judgeSkip", "find");
-
-    if(status==1||status==3||status==5){
+    	if(user_cookie == null || user_cookie == ''){
+        window.location.href="enter.html";         //判断是否为游客并跳转登陆界面
+    }
+    else if(status==1||status==3||status==5){
         window.location.href="phy_examine.html";
     }else if(status==2||status==4||status==6){
         window.location.href="hos_examine.html";
