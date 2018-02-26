27572824f4b744544d2aaee6769569ec7923f7be
wangxingchuan <wangxingchuan@ixincheng.com>
2018-02-26 15:34:49 +0800

    学习播放历史的跳转页面

--- wechat/src/main/webapp/xcview/js/my_study.js
+++ wechat/src/main/webapp/xcview/js/my_study.js
@@ -10,6 +10,7 @@ $(function(){
 		}
 
 	})	
+
 //	点击清除播放历史弹出确认取消框
 	$(".play-title p").click(function(){
 		$(".history_bg").show();
@@ -47,10 +48,44 @@ $(function(){
 		})	
 	
 
+
+
+
+
+
+
+
+
+
+
 })
 
 
+//搜索历史播放
+function go_play_hos(id){
+	requestService("/xczh/course/details?courseId="+id,null,function(data) {
 
+		var course = data.resultObject;
+		if(course.watchState == 2||course.watchState == 3 || course.watchState == 1){
+			if(course.type==1||course.type==2){
+				if(course.collection){
+//					专辑视频音频播放页
+				location.href="live_select_album.html?course_id="+id					
+				}else{
+//					单个视频音频播放
+				location.href="live_audio.html?my_study="+id					
+				}
+			}else if(course.type==3){
+//					播放页面
+				location.href="live_audio.html?my_study="+id									
+			}else{
+//					线下课页面
+				location.href="live_class.html?my_study="+id									
+			}		
+		}
+	})
+}
+//搜索历史播放结束
 
 
 
