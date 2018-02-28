7cb050d06a0cb6ae13aa4497b7f28f6508d88e8a
yangxuan <453099975@qq.com>
2018-02-28 10:29:12 +0800

    修改cookie的认证

--- wechat/src/main/webapp/xcview/js/my_homepage.js
+++ wechat/src/main/webapp/xcview/js/my_homepage.js
@@ -82,3 +82,35 @@ var isNouser='<div class="header_img_right y">主播工作台 &nbsp;></div>'+
 $(".my_anchor").click(function(){
 	location.href="down_load.html"
 })
+
+
+/**
+ * cookie
+ * 返回 1000 有效   1002 过期，去登录页面  1005 过期且去完善信息
+ */
+function authenticationCooKie(){
+	var falg = 1000;
+	var user_cookie = cookie.get("_uc_t_");
+	var third_party_cookie = cookie.get("third_party_uc_t_");
+	if(!stringnull(user_cookie)){ //未登录
+		falg = 1002
+		if(stringnull(third_party_cookie)){   //用户用微信登录的但是没有绑定注册信息
+			falg = 1005;
+		}
+	}
+	return falg;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
