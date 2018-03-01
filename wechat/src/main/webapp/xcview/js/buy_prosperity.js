5030634aed9d25574d47b11a18edbca8dd2e892c
wangxingchuan <wangxingchuan@ixincheng.com>
2018-03-01 22:00:13 +0800

    修改购买成功后的猜你喜欢

--- wechat/src/main/webapp/xcview/js/buy_prosperity.js
+++ wechat/src/main/webapp/xcview/js/buy_prosperity.js
@@ -56,39 +56,7 @@ function jump(){
 /**
  * 猜你喜欢
  */
-function guesYouLike(type,courseId){
-//	if(type == 1){ //直播间   直播中、即将直播、直播回放
-//		location.href="details.html?courseId="+courseId
-//	}else if(type ==2){ // 预告
-//		location.href="live_play.html?my_study="+courseId
-//	}else if(type ==3){ //课程页面
-//		location.href="live_audio.html?my_study="+courseId
-//	}else if(type ==4){ //线下培训班
-//		location.href="live_class.html?my_study="+courseId
-//	}else if(type ==5){ //专辑
-//		location.href="live_select_album.html?my_study="+courseId
-//	}
-	
-	
-//	<!--<a style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; z-index: 999;" href="javascript:;" onclick="jump({{item.id}})"></a>-->
-//	{{if item.type == 1 || item.type == 2}}
-//	<a style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; z-index: 999;" href="school_audio.html?course_id={{item.id}}"></a>
-//	{{else if item.type == 3}}
-//	<a style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; z-index: 999;" href="school_play.html?course_id={{item.id}}"></a>
-//	{{else if item.type == 4}}
-//	<a style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; z-index: 999;" href="school_class.html?course_id={{item.id}}"></a>
-//	
-//	{{/if}}
-	
-	if(type == 1 ||type ==2){ //课程
-		location.href="school_audio.html?course_id="+courseId
-	}else if(type ==3){ // 直播
-		location.href="school_play.html?course_id="+courseId
-	}else if(type ==4){ // 线下班
-		location.href="school_class.html?course_id="+courseId
-	}
-		
-}
+
 
 
 /**
