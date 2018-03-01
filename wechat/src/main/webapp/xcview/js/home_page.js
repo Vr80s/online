dc60b0f1a2f5bb0a14cf7c8588c94207e1a90123
wangxingchuan <wangxingchuan@ixincheng.com>
2018-03-01 14:26:52 +0800

    游客登录先到注册

--- wechat/src/main/webapp/xcview/js/home_page.js
+++ wechat/src/main/webapp/xcview/js/home_page.js
@@ -426,7 +426,7 @@ function clickBanner(id){
 var falg =authenticationCooKie();
 function go_study(){
 		if (falg==1002){
-			location.href ="/xcview/html/enter.html";		
+			location.href ="/xcview/html/cn_login.html";		
 		}else if (falg==1005) {
 			location.href ="/xcview/html/evpi.html";
 		}else{
