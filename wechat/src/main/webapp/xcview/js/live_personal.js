7e6e363914d56bf5e9a55b57598b604f5bf603aa
wangxingchuan <wangxingchuan@ixincheng.com>
2018-03-12 15:40:19 +0800

    主播详情的课程添加直播和即将直播跳转

--- wechat/src/main/webapp/xcview/js/live_personal.js
+++ wechat/src/main/webapp/xcview/js/live_personal.js
@@ -238,7 +238,91 @@ requestService("/xczh/host/hostPageCourse",{
 });
     refresh();
 });
-	
+//主播课程直播/即将直播跳转	
+
+function jump_play(id){
+   requestService("/xczh/course/details?courseId="+id,null,function(data) {
+      var userPlay=data.resultObject;
+      var falg =authenticationCooKie();       	       
+//收费的直播和即将直播未购买跳购买页    
+         if(userPlay.watchState==0 && userPlay.lineState==1){
+            location.href="school_play.html?course_id="+id 
+         }else if(userPlay.watchState==0 && userPlay.lineState==2){
+            location.href="school_play.html?course_id="+id          
+         }
+//免费的直播和即将直播跳直播间      
+         else if(userPlay.watchState==1 && userPlay.lineState==1){
+            if (falg==1002){
+            location.href ="/xcview/html/cn_login.html";      
+            }else if (falg==1005) {
+               location.href ="/xcview/html/evpi.html";
+            }else{
+            requestService("/xczh/history/add",
+               {courseId:id}
+               ,function(data) {
+      
+               }) 
+            location.href="details.html?courseId="+id
+            }
+         }else if(userPlay.watchState==1 && userPlay.lineState==2){
+            if (falg==1002){
+                  location.href ="/xcview/html/cn_login.html";      
+               }else if (falg==1005) {
+                  location.href ="/xcview/html/evpi.html";
+               }else{
+                  requestService("/xczh/history/add",
+                     {courseId:id}
+                     ,function(data) {
+            
+                     }) 
+                  location.href="details.html?courseId="+id  
+               }
+         }
+//购买后的直播和即将直播跳直播间
+         else if(userPlay.watchState==2 && userPlay.lineState==1){
+            requestService("/xczh/history/add",
+               {courseId:id}
+               ,function(data) {
+      
+               }) 
+            location.href="details.html?courseId="+id           
+         }else if(userPlay.watchState==2 && userPlay.lineState==2){
+            requestService("/xczh/history/add",
+               {courseId:id}
+               ,function(data) {
+      
+               }) 
+            location.href="details.html?courseId="+id  
+           }
+//主播本人自己的直播和即将直播跳直播间			
+			else if(userPlay.watchState==3 && userPlay.lineState==1){
+				requestService("/xczh/history/add",
+					{courseId:id}
+					,function(data) {
+		
+					})	
+				location.href="details.html?courseId="+id				
+			}else if(userPlay.watchState==3 && userPlay.lineState==2){
+				requestService("/xczh/history/add",
+					{courseId:id}
+					,function(data) {
+		
+					})	
+				location.href="details.html?courseId="+id				
+			}
+			else{
+				location.href="school_play.html?course_id="+id				
+			}
+
+ }
+ )}
+
+
+
+
+
+
+
 //	点击关注判断
 	function my_follow(followed,type){
 					requestService("/xczh/myinfo/updateFocus",{
