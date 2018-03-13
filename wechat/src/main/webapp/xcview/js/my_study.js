a539e8c78d97a5a68819e9c9404f99ef5dced33d
wangxingchuan <wangxingchuan@ixincheng.com>
2018-03-13 16:52:18 +0800

    学习，无学习课程时，显示图片

--- wechat/src/main/webapp/xcview/js/my_study.js
+++ wechat/src/main/webapp/xcview/js/my_study.js
@@ -44,11 +44,16 @@ var all_history="";
 	})	
 		
 //已购课程/结束课程	
-					
+
 //	var no_class='<p style="color: #a5a5a5;">暂无课程...</p>'
 	requestService("/xczh/myinfo/list",null,function(data) {
 			$(template("data_my_class",{items:data.resultObject[0]})).appendTo("#my_class_box");
 			$(template("data_my_class",{items:data.resultObject[1]})).appendTo("#my_class_box");
+			if(data.resultObject[0].courseList.length==0){				
+				$("#my_class_box").hide()
+				$(".wrap_noClass").show();
+			}
+			
 //点击播放视频后才开始记录播放历史	
 //直播中
 	$(".paly_ing_all").click(function(){
