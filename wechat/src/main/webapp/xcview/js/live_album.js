39b981e210ffc8d756fe5cb9ee2b752dc6d918ec
wangxingchuan <wangxingchuan@ixincheng.com>
2018-03-13 22:03:28 +0800

    主播简介判断字长度

--- wechat/src/main/webapp/xcview/js/live_album.js
+++ wechat/src/main/webapp/xcview/js/live_album.js
@@ -85,7 +85,26 @@ function stripHTML(str){
 		}else{
 			$(".wrap1 p").html(data.resultObject.lecturerDescription)
 		}
+			//判断简介的字长度
+		var h=$(".wrap1").height();
+		if(h>200){
+			$(".zhezhao1").show()
+			$(".btn1").show()
+			$(".wrap1").css({"height":"2rem","overflow":"hidden"})
+		}else{
+			$(".zhezhao1").hide()
+			$(".btn1").hide()
+		}
 		
+		var h2=$(".wrap").height();
+		if(h2>200){
+			$(".zhezhao").show()
+			$(".btn").show()
+			$(".wrap").css({"height":"2rem","overflow":"hidden"})
+		}else{
+			$(".zhezhao").hide()
+			$(".btn").hide()
+		}
 		
 		
 	});
