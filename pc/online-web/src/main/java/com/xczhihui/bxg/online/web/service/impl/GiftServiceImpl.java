fedd6348705b385d042699baecbdc3ed4f8088af
yuruixin <yuruixin_china@163.com>
2018-03-17 23:53:42 +0800

    刷礼物余额返回bug

--- pc/online-web/src/main/java/com/xczhihui/bxg/online/web/service/impl/GiftServiceImpl.java
+++ pc/online-web/src/main/java/com/xczhihui/bxg/online/web/service/impl/GiftServiceImpl.java
@@ -3,6 +3,7 @@ package com.xczhihui.bxg.online.web.service.impl;
 import com.xczhihui.bxg.online.api.po.Gift;
 import com.xczhihui.bxg.online.api.service.GiftSendService;
 import com.xczhihui.bxg.online.api.service.GiftService;
+import com.xczhihui.bxg.online.api.service.UserCoinService;
 import com.xczhihui.bxg.online.common.enums.OrderFrom;
 import com.xczhihui.bxg.online.web.dao.GiftDao;
 import org.slf4j.Logger;
@@ -22,6 +23,8 @@ public class GiftServiceImpl implements GiftService {
 	private GiftDao giftDao;
 	@Autowired
 	private GiftSendService giftSendService;
+	@Autowired
+	private UserCoinService userCoinService;
 
 	@Override
 	public Map<String,Object> addGiftStatement(String giverId, String receiverId, String giftId, OrderFrom orderFrom,int count,String liveId) {
@@ -30,8 +33,9 @@ public class GiftServiceImpl implements GiftService {
 		}
 		//每次送一个  20180305 yuxin
 		count=1;
-		Map<String, Object> stringObjectMap = giftSendService.addGiftStatement4Lock(liveId, giverId, receiverId, giftId, orderFrom, count, liveId);
-		return stringObjectMap;
+		Map<String, Object> map = giftSendService.addGiftStatement4Lock(liveId, giverId, receiverId, giftId, orderFrom, count, liveId);
+		map.put("balanceTotal",userCoinService.getBalanceByUserId(giverId));
+		return map;
 	}
 
 	@Override
