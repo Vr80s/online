9711ef717f3d78de81a35baf35dcf04c1c28c4a7
yuruixin <yuruixin_china@163.com>
2018-03-19 20:32:50 +0800

    解决资源转码超时动作 课程申请编辑漏洞。。

--- pc/medical-provider/src/test/java/com/xczhihui/medical/anchor/service/impl/CourseApplyServiceImplTest.java
+++ pc/medical-provider/src/test/java/com/xczhihui/medical/anchor/service/impl/CourseApplyServiceImplTest.java
@@ -1,3 +1,11 @@
+package com.xczhihui.medical.anchor.service.impl;
+
+import com.xczhihui.medical.anchor.service.IAnchorInfoService;
+import com.xczhihui.medical.anchor.service.ICourseApplyService;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import test.BaseJunit4Test;
+
 import static org.junit.Assert.*;
 
 /**
@@ -6,6 +14,13 @@ import static org.junit.Assert.*;
  * @author: name：yuxin <br>email: yuruixin@ixincheng.com <br>
  * Create Time:  2018/3/19 0019-下午 8:11<br>
  */
-public class CourseApplyServiceImplTest {
+public class CourseApplyServiceImplTest extends BaseJunit4Test {
+
+    @Autowired
+    private ICourseApplyService service;
+    @Test
+    public void updateCourseApplyResource() throws Exception {
+        service.updateCourseApplyResource();
+    }
 
 }
