3b34391faef12a7b35e3f2c5d926a3314634cce8
yuruixin <yuruixin_china@163.com>
2018-03-19 20:31:47 +0800

    修改获取礼物数目方法

---
+++ pc/medical-provider/src/test/java/com/xczhihui/medical/anchor/service/impl/CourseApplyServiceImplTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+
+/**
+ * Description: <br>
+ *
+ * @author: name：yuxin <br>email: yuruixin@ixincheng.com <br>
+ * Create Time:  2018/3/19 0019-下午 8:11<br>
+ */
+public class CourseApplyServiceImplTest {
+
+}
