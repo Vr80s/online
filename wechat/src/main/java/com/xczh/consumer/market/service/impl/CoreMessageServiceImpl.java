a7b209271aaca10d73968650d3aedafba9882c7c
yangxuan <453099975@qq.com>
2018-03-23 23:08:51 +0800

    修改bug

--- wechat/src/main/java/com/xczh/consumer/market/service/impl/CoreMessageServiceImpl.java
+++ wechat/src/main/java/com/xczh/consumer/market/service/impl/CoreMessageServiceImpl.java
@@ -1,6 +1,8 @@
 package com.xczh.consumer.market.service.impl;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,6 +14,8 @@ import com.xczh.consumer.market.controller.user.XzUserController;
 import com.xczh.consumer.market.service.CoreMessageService;
 import com.xczh.consumer.market.utils.MessageConstant;
 import com.xczh.consumer.market.utils.MessageUtil;
+import com.xczh.consumer.market.wxmessage.resp.Article;
+import com.xczh.consumer.market.wxmessage.resp.NewsMessage;
 import com.xczh.consumer.market.wxmessage.resp.TextMessage;
 
 @Service
@@ -41,10 +45,22 @@ public class CoreMessageServiceImpl implements CoreMessageService {
             textMessage.setMsgType(MessageConstant.RESP_MESSAGE_TYPE_TEXT);  
             textMessage.setFuncFlag(0);  
             
+            
+            
             // 文本消息  
             if (msgType.equals(MessageConstant.REQ_MESSAGE_TYPE_TEXT)) {  
                 // 接收用户发送的文本消息内容  
                 String content = requestMap.get("Content");  
+                
+                NewsMessage newsMessage = new NewsMessage();  
+                newsMessage.setToUserName(fromUserName);  
+                newsMessage.setFromUserName(toUserName);  
+                newsMessage.setCreateTime(new Date().getTime());  
+                newsMessage.setMsgType(MessageConstant.RESP_MESSAGE_TYPE_NEWS);  
+                newsMessage.setFuncFlag(0);  
+                
+                List<Article> articleList = new ArrayList<Article>();  
+                
                 if ("1".equals(content)) {  
                     textMessage.setContent("1是很好的");  
                     // 将文本消息对象转换成xml字符串  
@@ -53,6 +69,19 @@ public class CoreMessageServiceImpl implements CoreMessageService {
                     textMessage.setContent("我不是2货");  
                     // 将文本消息对象转换成xml字符串  
                     respMessage = MessageUtil.textMessageToXml(textMessage);  
+                }else if("3".equals(content)){
+                	  Article article = new Article();  
+                      article.setTitle("啦啦啦啦啦啦啦啦，我是卖报的小画家。");  
+                      article.setDescription("啦啦啦啦啦啦啦啦，我是卖报的小画家。");  
+                      article.setPicUrl("http://test-file.ipandatcm.com/18323230451/3654b4749a2b88f24ee6.jpg");  
+                      article.setUrl("https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx48d230a99f1c20d9&redirect_uri=http://test-wx.ixincheng.com/xczh/wxpublic/publicToRecommended&response_type=code&scope=snsapi_userinfo&state=STATE%23wechat_redirect&connect_redirect=1#wechat_redirect");  
+                      articleList.add(article);  
+                      // 设置图文消息个数  
+                      newsMessage.setArticleCount(articleList.size());  
+                      // 设置图文消息包含的图文集合  
+                      newsMessage.setArticles(articleList);  
+                      // 将图文消息对象转换成xml字符串  
+                      respMessage = MessageUtil.newsMessageToXml(newsMessage);  
                 }  
             }else if(msgType.equals(MessageConstant.EVENT_TYPE_SUBSCRIBE)){
             	
