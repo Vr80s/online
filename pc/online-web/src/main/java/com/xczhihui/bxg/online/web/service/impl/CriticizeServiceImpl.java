713d0daad3be84f4e6c8711ec85231b5e298ef7c
yuruixin <yuruixin_china@163.com>
2018-04-11 12:14:23 +0800

    删除考试历史代码

--- pc/online-web/src/main/java/com/xczhihui/bxg/online/web/service/impl/CriticizeServiceImpl.java
+++ pc/online-web/src/main/java/com/xczhihui/bxg/online/web/service/impl/CriticizeServiceImpl.java
@@ -8,15 +8,6 @@ import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
-
-
-
-
-
-
-
-
 import com.xczhihui.bxg.common.util.bean.Page;
 import com.xczhihui.bxg.online.api.service.CriticizeService;
 import com.xczhihui.bxg.online.api.vo.CriticizeVo;
