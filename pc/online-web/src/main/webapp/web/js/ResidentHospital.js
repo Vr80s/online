9173f1570f2dc83420faa941831121b052b30905
wangxingchuan <695014849@qq.com>
2018-04-11 09:21:05 +0800

    修改医馆的图片

--- pc/online-web/src/main/webapp/web/js/ResidentHospital.js
+++ pc/online-web/src/main/webapp/web/js/ResidentHospital.js
@@ -539,8 +539,9 @@ function picUpdown2(baseurl, imgname) {
 		$('#hos_Administration #hos_pic').removeClass('hide');
 		var picStr =
 			'<div style="position: relative;">' +
-			'<span style="position: absolute;top: 5px;right: 5px;color:red" class="hospic_del">X</span>' +
-			'<img src="' + data.resultObject + '?imageView2/1/w/260/h/147'+'" >' +
+			'<span style="position: absolute;top: 5px;right: 5px;color:red;z-index:5;" class="hospic_del">X</span>' +
+//			'<img src="' + data.resultObject + '?imageView2/1/w/260/h/147'+'" >' +
+			'<img src="' + data.resultObject + '?imageMogr2/thumbnail/260x147<'+'" >' +
 			'</div>'
 		$('#hos_Administration #hos_pic').append(picStr);
 	})
