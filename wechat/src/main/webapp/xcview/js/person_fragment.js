057032a98838224806977db53c5f045a318567c3
haozhongpin <850180305@qq.com>
2018-04-12 10:50:59 +0800

    ios引用测试。--直播间-文档区域显示隐藏

--- wechat/src/main/webapp/xcview/js/person_fragment.js
+++ wechat/src/main/webapp/xcview/js/person_fragment.js
@@ -53,34 +53,48 @@ requestService("/xczh/common/richTextDetails",{
 	type:type,
 	typeId:typeId
 },function(data) {
-	
 	if(type ==1 ){ // 展示页面  课程详情
 		if(data.resultObject==''||data.resultObject==null){
 			$("#jieshao").hide()			
 		}else{
 			$(".user_mywrite").html(data.resultObject);
 		}
+
+		
+
 	}else if(type == 2){ //主讲人  --》 直播间主讲人
 		if(data.resultObject==''||data.resultObject==null){
 			$("#jieshao").hide()			
 		}else{
 			$(".user_mywrite").html(data.resultObject);
 		}
+
+
 	}else if(type == 3){  //展示页面  主讲人
 		if(data.resultObject==''||data.resultObject==null){
 			$("#jieshao").hide()			
 		}else{
 			$(".user_mywrite").html(data.resultObject);
 		}
+
+		
+
 	}else if(type == 4){  //主播详情页面 --》 介绍 
 		if(data.resultObject==''||data.resultObject==null){
 			$("#jieshao").hide()			
 		}else{
 			$(".user_mywrite").html(data.resultObject);
 		}
+
+		
+
 	}
+
+
+
 	
-	
+
+
 	
 })
 
