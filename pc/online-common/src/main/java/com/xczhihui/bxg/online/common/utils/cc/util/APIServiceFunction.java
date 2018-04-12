6ebe9cfd0cff8cf83341efda51e79d93cbda5bcc
wangyishuai <15210815880@163.com>
2018-04-12 10:35:06 +0800

    文件断点续传

--- pc/online-common/src/main/java/com/xczhihui/bxg/online/common/utils/cc/util/APIServiceFunction.java
+++ pc/online-common/src/main/java/com/xczhihui/bxg/online/common/utils/cc/util/APIServiceFunction.java
@@ -1,15 +1,13 @@
 package com.xczhihui.bxg.online.common.utils.cc.util;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.URLEncoder;
+import java.io.*;
+import java.net.*;
+import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.TreeMap;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.io.IOUtils;
 
 /**
  * 功能：api接口公用函数
@@ -107,6 +105,178 @@ public class APIServiceFunction {
 			e.printStackTrace();
 		}
 		return document.toString();
+	}
+
+	//获取文件的MD5值
+	public static String getMd5File(File file){
+		String md5 = null;
+		try {
+			FileInputStream fis= new FileInputStream(file);
+			md5 = DigestUtils.md5Hex(IOUtils.toByteArray(fis));
+			IOUtils.closeQuietly(fis);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return md5;
+	}
+
+	/**
+	 * 向指定URL发送GET方法的请求
+	 *
+	 * @param url
+	 *            发送请求的URL
+	 * @param param
+	 *            请求参数，请求参数应该是 name1=value1&name2=value2 的形式。
+	 * @return URL 所代表远程资源的响应结果
+	 */
+	public static String sendGet(String url, String param) {
+		String result = "";
+		BufferedReader in = null;
+		try {
+			String urlNameString = url + "?" + param;
+			URL realUrl = new URL(urlNameString);
+			// 打开和URL之间的连接
+			URLConnection connection = realUrl.openConnection();
+			// 设置通用的请求属性
+			connection.setRequestProperty("accept", "*/*");
+			connection.setRequestProperty("connection", "Keep-Alive");
+			connection.setRequestProperty("user-agent",
+					"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+			// 建立实际的连接
+			connection.connect();
+			// 获取所有响应头字段
+			Map<String, List<String>> map = connection.getHeaderFields();
+			// 遍历所有的响应头字段
+			for (String key : map.keySet()) {
+				System.out.println(key + "--->" + map.get(key));
+			}
+			// 定义 BufferedReader输入流来读取URL的响应
+			in = new BufferedReader(new InputStreamReader(
+					connection.getInputStream()));
+			String line;
+			while ((line = in.readLine()) != null) {
+				result += line;
+			}
+		} catch (Exception e) {
+			System.out.println("发送GET请求出现异常！" + e);
+			e.printStackTrace();
+		}
+		// 使用finally块来关闭输入流
+		finally {
+			try {
+				if (in != null) {
+					in.close();
+				}
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		return result;
+	}
+
+
+	public static byte[] readChunk(File file) throws IOException {
+		if(file == null) {
+			throw new IOException("The file does not exist");
+		}
+		long fileLength = file.length();
+		long chunkStart=0;
+		if(chunkStart >= fileLength) {
+			throw new IOException("Start position > file length");
+		}
+		RandomAccessFile accessFile = null;
+		try {
+			long chunkEnd = file.length()-1;
+			int chunksLen = (int) (chunkEnd - chunkStart + 1);
+			byte[] chunks = new byte[chunksLen];
+			accessFile = new RandomAccessFile(file, "r");
+			accessFile.seek(chunkStart);
+			int readLength = accessFile.read(chunks, 0, chunksLen);
+			System.out.println("read Length: " + readLength);
+			accessFile.close();
+			return chunks;
+		} finally {
+			if(accessFile != null) {
+				try {
+					accessFile.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+
+	/* url为/servlet/uploadchunk?ccvid=&format= */
+	/* chunkStart为chunk起始位置*/
+	/* chunkEnd为chunk结束位置*/
+	/* file为文件*/
+	/* bufferOut为实际文件输出二进制内容*/
+
+	public static String uploadchunk(String url, int chunkStart, int chunkEnd, File file) {
+		byte[] bufferOut = null;
+		try {
+			bufferOut = readChunk(file);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		if(bufferOut == null) {
+			System.out.println("---------------read file chunk error-----------------");
+			return "read file error";
+		}
+		HttpURLConnection conn = null;
+		try {
+			String BOUNDARY = "---------CCHTTPAPIFormBoundaryEEXX" + new Random().nextInt(65536); // 定义数据分隔线
+			URL openUrl = new URL(url);
+			conn = (HttpURLConnection)openUrl.openConnection();
+			// 发送POST请求必须设置如下两行
+			conn.setDoOutput(true);
+			conn.setDoInput(true);
+			conn.setUseCaches(false);
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("connection", "Keep-Alive");
+			conn.setRequestProperty("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4)");
+			conn.setRequestProperty("Charsert", "UTF-8");
+			conn.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + BOUNDARY);
+			// content-range
+			conn.setRequestProperty("Content-Range", "bytes " + chunkStart + "-" + chunkEnd + "/" + file.length());
+			System.out.println("bytes " + chunkStart + "-" + chunkEnd + "/" + file.length());
+
+			DataOutputStream out = new DataOutputStream(conn.getOutputStream());
+			StringBuilder sb = new StringBuilder();
+			sb.append("--").append(BOUNDARY).append("\r\n");
+			sb.append("Content-Disposition: form-data;name=\"file" + file.getName() + "\";filename=\"" + file.getName()
+					+ "\"\r\n");
+			sb.append("Content-Type: application/octet-stream\r\n");
+			sb.append("\r\n");
+			byte[] data = sb.toString().getBytes();
+			out.write(data);
+			out.write(bufferOut);
+			out.write("\r\n".getBytes());
+			// 定义最后数据分隔线
+			byte[] end_data = ("--" + BOUNDARY + "--\r\n").getBytes();
+			out.write(end_data);
+			out.flush();
+			out.close();
+
+			// 定义BufferedReader输入流来读取URL的响应
+			BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			StringBuffer resultBuf = new StringBuffer("");
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				resultBuf.append(line);
+			}
+			reader.close();
+			conn.disconnect();
+			return resultBuf.toString();
+		} catch (Exception e) {
+			System.out.println("发送POST请求出现异常！" + e);
+			e.printStackTrace();
+		} finally {
+			if (conn != null)
+				conn.disconnect();
+		}
+		return null;
 	}
 
 }
