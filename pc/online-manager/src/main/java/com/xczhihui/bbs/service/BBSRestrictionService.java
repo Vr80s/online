77fa95098a6935c740580fd87366e11aac742bf7
hejiwei <bone58@sina.com>
2018-04-18 15:20:11 +0800

    [enhancement]优化 后台bbs禁言和拉黑的操作

--- pc/online-manager/src/main/java/com/xczhihui/bbs/service/BBSRestrictionService.java
+++ pc/online-manager/src/main/java/com/xczhihui/bbs/service/BBSRestrictionService.java
@@ -1,13 +1,40 @@
 package com.xczhihui.bbs.service;
 
+import java.util.List;
+
 import com.xczhihui.bxg.common.util.bean.ResponseObject;
 import com.xczhihui.utils.TableVo;
 
+/**
+ * 用户禁言拉黑服务类
+ *
+ * @author hejiwei
+ */
 public interface BBSRestrictionService {
 
+    /**
+     * 用户列表
+     *
+     * @param tableVo 搜索条件封装
+     * @return 列表数据
+     */
     TableVo list(TableVo tableVo);
 
-    ResponseObject updateGags(String userId, boolean gags);
+    /**
+     * 批量禁言/取消禁言
+     *
+     * @param userIds 用户id列表
+     * @param gags    禁言状态
+     * @return 操作结果
+     */
+    ResponseObject updateGags(List<String> userIds, boolean gags);
 
-    ResponseObject updateBlacklist(String userId, boolean blacklist);
+    /**
+     * 批量拉黑/取消拉黑
+     *
+     * @param userIds   用户id列表
+     * @param blacklist 拉黑状态
+     * @return 操作结果
+     */
+    ResponseObject updateBlacklist(List<String> userIds, boolean blacklist);
 }
