3bdc5b9daea752241d1355608f4a8f53d50b4f5a
hejiwei <bone58@sina.com>
2018-04-18 18:30:16 +0800

    [bug fix]fix 后台头条管理查询遗留bug

--- pc/online-manager/src/main/java/com/xczhihui/headline/dao/ArticleDao.java
+++ pc/online-manager/src/main/java/com/xczhihui/headline/dao/ArticleDao.java
@@ -1,6 +1,8 @@
 package com.xczhihui.headline.dao;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Repository;
@@ -17,28 +19,23 @@ import com.xczhihui.headline.vo.ArticleVo;
 @Repository("articleDao")
 public class ArticleDao extends HibernateDao<ArticleVo> {
 
-    public Page<ArticleVo> findCloudClassCoursePage(ArticleVo articleVo,
-                                                    int currentPage, int pageSize) {
-        Map<String, Object> paramMap = new HashMap<>();
-        StringBuilder sql = new StringBuilder(
-                "SELECT \n"
-                        + "  article.id,article.title,article.content,article.type_id,article.img_path,article.banner_path,article.browse_sum,\n"
-                        + "  article.praise_sum,article.comment_sum,article.banner_status,article.status,article.is_delete,article.user_id,\n"
-                        + "  article.create_time,article.praise_login_names,article.update_time,article.url,article.recommend_time, \n"
-                        + "  GROUP_CONCAT(tag.name) AS tagName,\n"
-                        + "  GROUP_CONCAT(tag.id) AS tagId,\n"
-                        + "  arttype.`name` AS typeName,\n"
-                        + "  article.`user_id` author,\n"
-                        + " if(article.recommend_time< now(),0,article.sort) sort, "
-                        + "  (SELECT md.name FROM `medical_doctor_author_article` mdaa JOIN medical_doctor md ON mdaa.`doctor_id` = md.`id` WHERE mdaa.`article_id` = article.`id`) doctorAuthor,\n"
-                        + "  (SELECT GROUP_CONCAT(md.name) FROM `medical_doctor_report` mdr JOIN medical_doctor md ON mdr.`doctor_id` = md.`id` WHERE mdr.`article_id` = article.`id`) reportDoctor\n"
-                        + "FROM\n" + "  oe_bxs_article article,\n"
-                        + "  article_r_tag art,\n" + "  oe_bxs_tag tag,\n"
-                        + "  article_type arttype\n"
-                        + "WHERE article.id = art.article_id \n"
-                        + "  AND art.tag_id = tag.id \n"
-                        + "  AND article.type_id = arttype.id ");
-
+    public Page<ArticleVo> findArticlePage(ArticleVo articleVo,
+                                           int currentPage, int pageSize) {
+        Map<String, Object> paramMap = new HashMap<>(8);
+        StringBuilder sql = new StringBuilder(" SELECT " +
+                "    article.id,article.title,article.content,article.type_id,article.img_path,article.banner_path,article.browse_sum," +
+                "    article.praise_sum,article.comment_sum,article.banner_status,article.status,article.is_delete,article.user_id," +
+                "    article.create_time,article.praise_login_names,article.update_time,article.url,article.recommend_time," +
+                "    arttype.`name` AS typeName, article.`user_id` author, if(article.recommend_time< now(),0,article.sort) sort," +
+                "    art_tag.tagName as tagName, art_tag.tagId as tagId" +
+                "    FROM oe_bxs_article article" +
+                "       left join (" +
+                "           select GROUP_CONCAT(t.name) as tagName, rt.article_id, group_concat(t.id) as tagId " +
+                "               from article_r_tag rt , oe_bxs_tag t" +
+                "           where rt.tag_id = t.id group by rt.article_id" +
+                "          ) art_tag on article.id = art_tag.article_id" +
+                "      left join article_type arttype on article.type_id = arttype.id " +
+                " where 1 = 1");
         if (articleVo.getTitle() != null) {
             sql.append(" and article.title like :title");
             paramMap.put("title", "%" + articleVo.getTitle() + "%");
@@ -59,10 +56,31 @@ public class ArticleDao extends HibernateDao<ArticleVo> {
             sql.append(" and article.create_time <=:stopTime");
             paramMap.put("stopTime", articleVo.getStopTime());
         }
-        sql.append(" GROUP BY article.id order by article.status desc,sort desc, create_time desc ");
+        sql.append(" order by article.status desc,sort desc, create_time desc ");
 
         return this.findPageBySQL(sql.toString(), paramMap,
                 ArticleVo.class, currentPage, pageSize);
     }
 
+    public String getDoctorAuthorByArticleId(Integer articleId) {
+        String sql = "SELECT group_concat(md.name) " +
+                " FROM `medical_doctor_special_column` mdsc " +
+                " JOIN medical_doctor md ON mdsc.`doctor_id` = md.`id`" +
+                " WHERE mdsc.`doctor_id` = md.`id` and mdsc.article_id = ?";
+        List<Object> params = new ArrayList<>(1);
+        params.add(articleId);
+        Object name = this.findUniqueBySql(sql, params);
+        return name != null ? name.toString() : null;
+    }
+
+    public String getReportAuthorByArticleId(Integer articleId) {
+        String sql = "SELECT group_concat(md.name) " +
+                " FROM `medical_doctor_report` mdr " +
+                " JOIN medical_doctor md ON mdr.`doctor_id` = md.`id`" +
+                " WHERE mdr.`doctor_id` = md.`id` and mdr.article_id = ?";
+        List<Object> params = new ArrayList<>(1);
+        params.add(articleId);
+        Object name = this.findUniqueBySql(sql, params);
+        return name != null ? name.toString() : null;
+    }
 }
