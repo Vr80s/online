63dccd07cddcf61ead42e9d2b6ed715c91227fd8
MSI\yangxuan <453099975@qq.com>
2018-04-24 22:13:23 +0800

    bug--增加banner图的跳转

--- wechat/src/main/webapp/xcview/js/common/common.js
+++ wechat/src/main/webapp/xcview/js/common/common.js
@@ -37,7 +37,6 @@ function getQueryString(name) {
     var search = decodeURIComponent(window.location.search);
     var r = search.substr(1).match(reg);
     if (r != null) return unescape(r[2]); return null;
-    
 }
 
 /*
