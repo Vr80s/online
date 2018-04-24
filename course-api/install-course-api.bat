65589fee306cbe1248b5bfc1ce1da886513cb1b1
yuruixin <yuruixin_china@163.com>
2018-04-24 20:19:58 +0800

    为将wechat-api、wechat-provider更名为course-api、course-provider

--- course-api/install-course-api.bat
+++ course-api/install-course-api.bat
@@ -1,5 +1,5 @@
 @echo off
 
-echo build user-center-provider
-cd pc\user-center-provider
+echo build course-api
+cd course-api
 if %env% == 0 ( mvn clean install -Pdev-local) else if %env% == 1 ( mvn clean install -Pdev) else if %env% == 2 ( mvn clean install -Ptest) else if %env% == 3 ( mvn clean install -Pprod) else (echo env error)
