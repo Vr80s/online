b5b7453b21a5c3e0fddcc542f27cfa99795d5783
haozhongpin <850180305@qq.com>
2018-04-26 21:52:11 +0800

    专辑--学习跳转。点击专辑banner

--- wechat/src/main/webapp/xcview/js/live_select_album.js
+++ wechat/src/main/webapp/xcview/js/live_select_album.js
@@ -107,6 +107,7 @@ $(".my_details li").click(function(){
 
 var courseId = getQueryString('course_id');
 var course='';
+var collections="";
 	requestService("/xczh/course/getCoursesByCollectionId",{
 		collectionId : courseId	
 	},function(data) {
@@ -118,18 +119,27 @@ var course='';
 				data.resultObject[i].collectionId=courseId;
 			}
 			course=data.resultObject[0];   //获取专辑id第一个
+            collections=data.resultObject;  //获取所有专辑
 			$("#select_album").html(template('data_select_album',{items:data.resultObject}));		
 		}
 	})
 //点击视频默认第一个视频ID
-		
+
+    
+	
 	function btn_album_page(){                       //下面id是获取专辑id
-		location.replace("live_album.html?course_id="+course.id+"&direct_id="+course.directId+"&collection_id="+courseId+"&name_title="+name_title+"&index=0");
+        var courseIndex = localStorage.getItem("course"+courseId);
+        if (courseIndex==null||courseIndex =="") {
+            courseIndex=0
+        }
+        var collection = collections[courseIndex];
+		location.replace("live_album.html?course_id="+collection.id+"&direct_id="+collection.directId+"&collection_id="+courseId+"&name_title="+name_title+"&index="+courseIndex);
 	}
 //选集视频跳转
 function jump_album_my(e,selectId,selectDirectId){
 	//alert(e.target)
 	var index = e.parentNode.value
+    localStorage.setItem('course'+courseId, index);
 	location.replace("live_album.html?course_id="+selectId+"&direct_id="+selectDirectId+"&collection_id="+courseId+"&name_title="+name_title+"&index="+index);
 }
 function refresh(){
