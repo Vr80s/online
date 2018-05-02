eace188f61e2a97967935a76b94e834902dd140c
hejiwei <bone58@sina.com>
2018-05-02 14:30:35 +0800

    [bug fix]fix 异常引入

--- pc/medical-api/src/main/java/com/xczhihui/medical/exception/AnchorException.java
+++ pc/medical-api/src/main/java/com/xczhihui/medical/exception/AnchorException.java
@@ -2,6 +2,8 @@ package com.xczhihui.medical.exception;
 
 import java.io.Serializable;
 
+import com.xczhihui.common.exception.IpandaTcmException;
+
 /**
  * Description：主播相关异常
  * creed: Talk is cheap,show me the code
