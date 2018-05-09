ce019e2a397a8ae3a07605f4e16438015db57313
MSI\yangxuan <453099975@qq.com>
2018-05-09 10:45:06 +0800

    修改banner回显的js方法

--- pc/online-manager/src/main/webapp/js/operate/mobileBanner2.js
+++ pc/online-manager/src/main/webapp/js/operate/mobileBanner2.js
@@ -370,8 +370,16 @@ function lalalalalalala(linkTypeValue){
  * @returns
  */
 function getValueByStr(search,name) {
-    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
-    var r = search.match(reg);
+//    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+//    var r = search.match(reg);
+//    if (r != null) return unescape(r[2]); return null;
+	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    var r = "";
+    if(search.indexOf("?")!=-1){
+       r = search.substr(search.indexOf("?")+1).match(reg);
+    }else{
+       r = search.match(reg);
+    }
     if (r != null) return unescape(r[2]); return null;
 }
 
