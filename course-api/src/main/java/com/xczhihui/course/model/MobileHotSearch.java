47ff42ebdd1941f23ec8e61f252883cb6f6be60a
wangyishuai <15210815880@163.com>
2018-05-10 10:54:58 +0800

    统一h5接口代码

---
+++ course-api/src/main/java/com/xczhihui/course/model/MobileHotSearch.java
@@ -0,0 +1,134 @@
+package com.xczhihui.course.model;
+
+import com.baomidou.mybatisplus.activerecord.Model;
+import com.baomidou.mybatisplus.annotations.TableField;
+import com.baomidou.mybatisplus.annotations.TableName;
+
+import java.io.Serializable;
+import java.util.Date;
+
+
+/**
+ * Description：移动端搜索实体类
+ * creed: Talk is cheap,show me the code
+ * @author name：wangyishuai <br>email: wangyishuai@ixincheng.com
+ * @Date: 2018/5/10 10:04
+ **/
+@TableName("oe_mobile_search")
+public class MobileHotSearch extends Model<MobileHotSearch> {
+
+    private static final long serialVersionUID = 1L;
+
+	private Integer id;
+
+	/**
+	 *名称
+	 */
+	@TableField("name")
+	private String name;
+
+	/**
+	 *创建人id
+	 */
+	@TableField("create_person")
+	private String createPerson;
+
+	/**
+	 *创建时间
+	 */
+	@TableField("create_time")
+	private Date createTime;
+
+	/**
+	 *状态，0禁用，1启用
+	 */
+	@TableField("status")
+	private Integer status;
+
+	/**
+	 *1 搜索框 2 热门搜索
+	 */
+	@TableField("search_type")
+	private String searchType;
+
+	/**
+	 *排序
+	 */
+	@TableField("seq")
+	private Integer seq;
+
+	/**
+	 *是否删除
+	 */
+	@TableField("is_delete")
+	private boolean isDelete;
+
+	@Override
+	protected Serializable pkVal() {
+		return null;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCreatePerson() {
+		return createPerson;
+	}
+
+	public void setCreatePerson(String createPerson) {
+		this.createPerson = createPerson;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getSearchType() {
+		return searchType;
+	}
+
+	public void setSearchType(String searchType) {
+		this.searchType = searchType;
+	}
+
+	public Integer getSeq() {
+		return seq;
+	}
+
+	public void setSeq(Integer seq) {
+		this.seq = seq;
+	}
+
+	public boolean isDelete() {
+		return isDelete;
+	}
+
+	public void setDelete(boolean delete) {
+		isDelete = delete;
+	}
+}
