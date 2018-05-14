0cede88d801dc13a2d117e0a221b1a2f7f4e405f
wangxingchuan <695014849@qq.com>
2018-05-14 10:46:06 +0800

    直播间添加关注增加时间

--- pc/online-web/src/main/webapp/web/js/liveVideo/liveVideoSDK.js
+++ pc/online-web/src/main/webapp/web/js/liveVideo/liveVideoSDK.js
@@ -101,6 +101,7 @@ getHostInfo();
 
 /*点击关注已关注开始*/
 $(".concern-click").click(function(){
+	$(".concern-click").attr("disabled","disabled");
 	var lalal = $(".concern-click").text();
 	if(lalal == "加关注"){
 		RequestService("/focus/updateFocus", "GET", {
@@ -110,8 +111,13 @@ $(".concern-click").click(function(){
 		});
 		$(".concern-right").css("background","#bbb");
 		$(".concern-right").html("已关注");
+		
+		setTimeout(function(){
+			$(".concern-click").removeAttr("disabled");
+		},5000)
 		getHostInfo();
 	} else{
+		$(".concern-click").attr("disabled","disabled");
 		RequestService("/focus/updateFocus", "GET", {
 			lecturerId:lecturerId,type:2
 		}, function(data) {
@@ -119,7 +125,11 @@ $(".concern-click").click(function(){
 		});
 		$(".concern-right").css("background","#00BC12");
 		$(".concern-right").html("加关注");
-		getHostInfo();
+	
+		setTimeout(function(){
+			$(".concern-click").removeAttr("disabled");
+		},5000)
+			getHostInfo();
 	}
 });
 /*点击关注已关注结束*/
