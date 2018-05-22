32c64f2cb6143a5b29688a7901bbe87b950791cd
MSI\yangxuan <453099975@qq.com>
2018-05-22 09:53:34 +0800

    修改reset前缀的方法支持事务、修改h5我的页面显示粉丝数和关注数bug

--- wechat/src/main/webapp/xcview/js/my_homepage.js
+++ wechat/src/main/webapp/xcview/js/my_homepage.js
@@ -50,16 +50,19 @@ function balance() {
     requestService("/xczh/manager/home",{
     },function(data) {
         if(data.success==true){
-            $("#xmbNumber").text(parseInt(data.resultObject.xmbCount));
-            $("#courseNumber").text(data.resultObject.courseCount);
+        	var baseInfo =  data.resultObject;
+        	
+            $("#xmbNumber").text(baseInfo.xmbCount);
+            $("#courseNumber").text(baseInfo.courseCount);
+            $("#attentionid").html(baseInfo.focusCount);
+       	    $("#fansid").html(baseInfo.fansCount);
             //用户头像
             //$(".header_img").html(template('userInfo',data.resultObject.user));
             if(stringnull(data.resultObject.user)){
             	 var item = data.resultObject.user;
             	 $("#smallHeadPhoto").attr("src",item.smallHeadPhoto + '?imageView2/2/w/160');
             	 $("#p_name").html(item.name);
-            	 $("#attentionid").html(item.name);
-            	 $("#fansid").html(item.name);
+            	 
             }
         }else{        	           
             webToast(data.errorMessage,"middle",1500);
