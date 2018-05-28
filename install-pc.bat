438174ef73d32b4d8bb13506aaf254d4eb6db3e4
yuruixin <yuruixin_china@163.com>
2018-05-28 20:53:17 +0800

    调整批处理

--- install-pc.bat
+++ install-pc.bat
@@ -4,19 +4,19 @@ set /p env=1.dev 2.test 3.prod 0.local-dev:
 if %env% == 0 ( set envName=你选择的是：本地开发环境) else if %env% == 1 (set envName= 你选择的是：开发环境) else if %env% == 2 (set envName= 你选择的是：测试环境) else if %env% == 3 (set envName= 你选择的是：生产环境) else (echo 环境变量选择有误)
 echo %envName%
 echo 当前路径：%cd%
-call pc\user-center-provider\install-user-center-provider.bat
-cd ..\..\
+call user-center-provider\install-user-center-provider.bat
+cd ..\
 echo 当前路径：%cd%
-call pc\medical-provider\install-medical-provider.bat
-cd ..\..\
+call medical-provider\install-medical-provider.bat
+cd ..\
 echo 当前路径：%cd%
 call course-provider\install-course-provider
 cd ..\
 echo 当前路径：%cd%
-call pc\online-web\install-online-web.bat
-cd ..\..\
+call online-web\install-online-web.bat
+cd ..\
 echo 当前路径：%cd%
-call pc\online-manager\install-online-manager.bat
-cd ..\..\
+call online-manager\install-online-manager.bat
+cd ..\
 echo 当前路径：%cd%
 pause
