593cd74c25351dae96ca60c89bd4c27a9c0ae75e
yuruixin <yuruixin_china@163.com>
2018-06-12 18:30:21 +0800

    修改批处理

--- online-manager/install-online-manager.bat
+++ online-manager/install-online-manager.bat
@@ -3,4 +3,4 @@
 echo build online-manager
 cd online-manager
 mvn clean
-#if %env% == 0 ( mvn clean install -Pdev-local) else if %env% == 1 ( mvn clean install -Pdev) else if %env% == 2 ( mvn clean install -Ptest) else if %env% == 3 ( mvn clean install -Pprod) else (echo env error)
+#if %env% == 0 ( mvn clean package -Pdev-local) else if %env% == 1 ( mvn clean package -Pdev) else if %env% == 2 ( mvn clean package -Ptest) else if %env% == 3 ( mvn clean package -Pprod) else (echo env error)
