7fdb493d325391db97c02c978b7d0e38a7ab9aaa
wangyishuai <15210815880@163.com>
2018-06-20 20:40:39 +0800

    医师动态添加，编辑，删除，列表

--- medical-provider/src/main/java/com/xczhihui/medical/doctor/service/impl/MedicalDoctorAccountServiceImpl.java
+++ medical-provider/src/main/java/com/xczhihui/medical/doctor/service/impl/MedicalDoctorAccountServiceImpl.java
@@ -1,9 +1,12 @@
 package com.xczhihui.medical.doctor.service.impl;
 
+import com.baomidou.mybatisplus.service.IService;
 import com.baomidou.mybatisplus.service.impl.ServiceImpl;
 import com.xczhihui.medical.doctor.mapper.MedicalDoctorAccountMapper;
 import com.xczhihui.medical.doctor.model.MedicalDoctorAccount;
 import com.xczhihui.medical.doctor.service.IMedicalDoctorAccountService;
+import com.xczhihui.medical.hospital.service.IMedicalHospitalApplyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,5 +19,12 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class MedicalDoctorAccountServiceImpl extends ServiceImpl<MedicalDoctorAccountMapper, MedicalDoctorAccount> implements IMedicalDoctorAccountService {
-	
+
+    @Autowired
+    private MedicalDoctorAccountMapper medicalDoctorAccountMapper;
+
+    @Override
+    public MedicalDoctorAccount getByUserId(String userId) {
+        return medicalDoctorAccountMapper.getByUserId(userId);
+    }
 }
