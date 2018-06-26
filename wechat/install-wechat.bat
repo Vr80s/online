548a334cd6146cf1ec060fc209c473786fe73f46
yuruixin <yuruixin_china@163.com>
2018-06-26 15:01:22 +0800

    修改批处理

--- wechat/install-wechat.bat
+++ wechat/install-wechat.bat
@@ -2,5 +2,4 @@
 
 echo build wechat
 cd wechat
-mvn clean
-#if %env% == 0 ( mvn clean install -Pdev-local) else if %env% == 1 ( mvn clean install -Pdev) else if %env% == 2 ( mvn clean install -Ptest) else if %env% == 3 ( mvn clean install -Pprod) else (echo env error)
+if %env% == 0 ( mvn clean install -Pdev-local) else if %env% == 1 ( mvn clean install -Pdev) else if %env% == 2 ( mvn clean install -Ptest) else if %env% == 3 ( mvn clean install -Pprod) else (echo env error)
