6030a2df360517c7239681555cd8cca29241e3a1
yuruixin <yuruixin_china@163.com>
2018-06-29 10:30:18 +0800

    修改批处理

--- medical-provider/install-medical-provider.bat
+++ medical-provider/install-medical-provider.bat
@@ -1,5 +1,5 @@
 @echo off
 
-echo build medical-provider
+echo build medical-provider %env%
 cd medical-provider
 if %env% == 0 ( mvn clean package -Pdev-local) else if %env% == 1 ( mvn clean package -Pdev) else if %env% == 2 ( mvn clean package -Ptest) else if %env% == 3 ( mvn clean package -Pprod) else (echo env error)
--- online-web/install-online-web.bat
+++ online-web/install-online-web.bat
@@ -1,5 +1,5 @@
 @echo off
 
-echo build online-web
+echo build online-web %env%
 cd online-web
 if %env% == 0 ( mvn clean package -Pdev-local) else if %env% == 1 ( mvn clean package -Pdev) else if %env% == 2 ( mvn clean package -Ptest) else if %env% == 3 ( mvn clean package -Pprod) else (echo env error)
