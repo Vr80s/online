770ea935ee05db95ff6498e952a7e228c1a6727c
yuruixin <yuruixin_china@163.com>
2018-07-02 09:36:40 +0800

    user-center-api  代码格式化

--- user-center-api/src/main/java/com/xczhihui/user/center/vo/OeUserVO.java
+++ user-center-api/src/main/java/com/xczhihui/user/center/vo/OeUserVO.java
@@ -1,88 +1,88 @@
 package com.xczhihui.user.center.vo;
 
-import lombok.Data;
-
 import java.io.Serializable;
 import java.util.Date;
+
+import lombok.Data;
 
 @Data
 public class OeUserVO implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-	private String id;
-	private String name;
-	private String loginName;
-	private String salt;
-	private String password;
-	private Integer sex;
-	private String mobile;
-	private String email;
-	private String smallHeadPhoto;
-	private Date createTime;
-	private Integer status;
-	private String lastLoginIp;
-	private Date lastLoginDate;
-	private Integer visitSum;
-	private Integer stayTime;
-	private Integer jobyears;
-	private Integer occupation;
-	private String regionId;
-	private String regionAreaId;
+    private String id;
+    private String name;
+    private String loginName;
+    private String salt;
+    private String password;
+    private Integer sex;
+    private String mobile;
+    private String email;
+    private String smallHeadPhoto;
+    private Date createTime;
+    private Integer status;
+    private String lastLoginIp;
+    private Date lastLoginDate;
+    private Integer visitSum;
+    private Integer stayTime;
+    private Integer jobyears;
+    private Integer occupation;
+    private String regionId;
+    private String regionAreaId;
     /**
      * 市
      */
-	private String regionCityId;
+    private String regionCityId;
     /**
      * 职业,其他
      */
-	private String occupationOther;
+    private String occupationOther;
     /**
      * 详细地址
      */
-	private String fullAddress;
+    private String fullAddress;
     /**
      * 三方用户唯一标识
      */
-	private String unionId;
+    private String unionId;
     /**
      * 三方账号绑定的账号ID
      */
-	private String refId;
+    private String refId;
     /**
      * 分享者id(上级用户)
      */
-	private String parentId;
+    private String parentId;
     /**
      * 分享码
      */
-	private String shareCode;
+    private String shareCode;
     /**
      * 变更时间
      */
-	private Date changeTime;
+    private Date changeTime;
     /**
      * 用户来源：1.pc 2.h5 3.android 4.ios 5.导入
      */
-	private String origin;
+    private String origin;
     /**
      * 微吼id
      */
-	private String vhallId;
+    private String vhallId;
     /**
      * 微吼密码
      */
-	private String vhallPass;
+    private String vhallPass;
     /**
      * 微吼登录名
      */
-	private String vhallName;
-	private String provinceName;
-	private String cityName;
+    private String vhallName;
+    private String provinceName;
+    private String cityName;
     /**
      * 地区的名字
      */
-	private String countyName;
-	private Boolean visitor;
+    private String countyName;
+    private Boolean visitor;
 
 }
