1538c904e674855c687b6a6225af934700cb0f22
MSI\yangxuan <453099975@qq.com>
2018-07-05 17:38:27 +0800

    修改bug5441

--- wechat/src/main/java/com/xczh/consumer/market/controller/doctor/DoctorController.java
+++ wechat/src/main/java/com/xczh/consumer/market/controller/doctor/DoctorController.java
@@ -6,9 +6,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.apache.solr.client.solrj.SolrServerException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
