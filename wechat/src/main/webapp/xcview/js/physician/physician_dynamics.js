8a87ff8c0254923fc160fdc2065adfaef9c6b2c9
haozhognping <haozhongping@ixincheng.com>
2018-07-17 18:00:47 +0800

    招生简章显示

--- wechat/src/main/webapp/xcview/js/physician/physician_dynamics.js
+++ wechat/src/main/webapp/xcview/js/physician/physician_dynamics.js
@@ -898,9 +898,19 @@ function createDoctorIntroduction(introduction){
 // 在线弟子申请的状态 1->未报名 2->没有审核 3->审核未通过 4->审核已通过 . 值是1与3 去提交信息页面 2与4 去申请信息查看页
 requestGetService("/xczh/host/doctor/apprentice",{doctorId:doctorId},function (data) {
     if (data.success == true) {
+
+        // 招生简章详情
+        $('.prose_origin_main').html(template('prose_origin_main_id', {items: data.resultObject.regulations}));
+
+        $(".prose_origin_details").click(function(){
+            var id=$(this).attr("data-ids");
+            var tokenStr=$(this).attr("data-tokenStr");
+            location.href ='inherited_introduction.html?merId='+id+tokenStr;
+        });
+
+
         // 获取判断是否提交申请弟子信息  师承底部判断是否提交
         $('.disciple_application_state').html(template('disciple_application_state_id', {items: data.resultObject}));
-
        // 判断--老师解惑--时隐藏
         if (!isNotBlank(data.resultObject.questions)) {
             $(".QA").hide();
@@ -913,15 +923,13 @@ requestGetService("/xczh/host/doctor/apprentice",{doctorId:doctorId},function (d
         
         // 弟子头像--显示
         if (!isNotBlank(data.resultObject.apprentices)) {
-            alert(11);
+            // alert(11);
             $(".disciple_main").hide();
         } else{
             $(".disciple_main").show();
              // 医师问答列表
             $('.disciple_main_id').html(template('disciple_main_id', {items: data.resultObject.apprentices}));
         }
-
-        // alert({items: data.resultObject.apprentices});
         // 获取头像长度做弟子总数  
         var length = $(".disciple_main_apprentices").size();
         $(".disciple_number").html(length);
