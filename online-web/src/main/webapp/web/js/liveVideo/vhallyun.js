52cde7d0f9e4d7e1d863a3ace9c09af45667d768
MSI\yangxuan <453099975@qq.com>
2018-08-08 22:45:33 +0800

    修改pc加载次数

--- online-web/src/main/webapp/web/js/liveVideo/vhallyun.js
+++ online-web/src/main/webapp/web/js/liveVideo/vhallyun.js
@@ -25,30 +25,30 @@ RequestService("/vhallyun/vhallYunToken","get",{channelId:vhallObj.channelId,roo
 var onOff = true, isFilter = true;
 
 $("#filter-msg").on("click", function() {//只看主办方消息   
-	    if (isFilter) {   //
-	        $(this).addClass("filter-yes").attr("title", "查看全部消息");
-	        for (var i = 0; i < $(".chatmsg li").length; i++) {
-	            var isRole = $(".chatmsg li").eq(i).data("role");
-	            if (isRole == "no") {
-	                $(".chatmsg li").eq(i).addClass("hide");
-	            }
-	        }
-	        $(".chartlist").mCustomScrollbar("update").mCustomScrollbar("scrollTo", "99999");
-	        
-	        //变成红色
-	        $("#filter-msg").removeClass("lecturer");
-	        $("#filter-msg").addClass("lecturer_filter1");
-	        isFilter = false;
-	    } else {
-	    	
-	        $(this).removeClass("filter-yes").attr("title", "只看主办方消息");
-	        $(".chatmsg li").removeClass("hide");
-	        $(".chartlist").mCustomScrollbar("update").mCustomScrollbar("scrollTo", "99999");
-	        
-	        $("#filter-msg").removeClass("lecturer_filter1");
-	        $("#filter-msg").addClass("lecturer");
-	        isFilter = true;
-	    }
+    if (isFilter) {   //
+        $(this).addClass("filter-yes").attr("title", "查看全部消息");
+        for (var i = 0; i < $(".chatmsg li").length; i++) {
+            var isRole = $(".chatmsg li").eq(i).data("role");
+            if (isRole == "no") {
+                $(".chatmsg li").eq(i).addClass("hide");
+            }
+        }
+        $(".chartlist").mCustomScrollbar("update").mCustomScrollbar("scrollTo", "99999");
+        
+        //变成红色
+        $("#filter-msg").removeClass("lecturer");
+        $("#filter-msg").addClass("lecturer_filter1");
+        isFilter = false;
+    } else {
+    	
+        $(this).removeClass("filter-yes").attr("title", "只看主办方消息");
+        $(".chatmsg li").removeClass("hide");
+        $(".chartlist").mCustomScrollbar("update").mCustomScrollbar("scrollTo", "99999");
+        
+        $("#filter-msg").removeClass("lecturer_filter1");
+        $("#filter-msg").addClass("lecturer");
+        isFilter = true;
+    }
 })
 
 
@@ -62,36 +62,15 @@ if(liveStatus == 1 || liveStatus == 3){
 
   
 function elsBind(){
-	
     window.doc = {};
-    
     var readyCallback = function(){
-       /**
-         * 开始推流事件监听
-         */
-        VhallLive.onPublishStart(function(){
-             console.log('直播开始！');
-             VhallLive.play();
-        })
-        /**
-         * 停止推流事件监听
-         */
-        VhallLive.onPublishStop(function(){
-            console.log('直播结束！');
-        })
-    	
       window.doc = new VhallDocPassive({
         channelId:vhallObj.channelId, //频道Id
         docNode:'my-doc-area'//文档显示节点div id
       });
-      
-      
       var liveType = (liveStatus == 1 ? "live" : "vod");
       var recordId = (liveStatus == 1 ? "" : vhallObj.recordId);
-        
-      
       //判断是回放呢，还是直播呢
-      
       VhallLive.init({
        roomId:vhallObj.roomId,
        recordId:recordId, //回放Id，点播必填，直播不写
@@ -101,103 +80,34 @@ function elsBind(){
           VhallLive.play();
        }
      });    
-        
     }
-
     window.Vhall.ready(readyCallback);
-    
-    
     //配置文档和直播
     window.Vhall.config({
          appId :vhallObj.appId,//应用 ID ,必填
          accountId :vhallObj.accountId,//第三方用户唯一标识,必填
          token:vhallObj.token//token必填
     });
-
     /**
      * 加载消息
      */
     setTimeout(function(){
-    
        window.Vhall.ready(function(){
-    	  /**
-         * 初始化聊天对象
-         */
         window.chat = new VhallChat({
            channelId:vhallObj.channelId //频道Id
         });
-
-        /**
-         * 监听聊天消息
-         */
-        window.chat.on(function(msg){
-            //在此收到聊天消息，消息内容为msg
-            if (msg){
-                var str = chatLoad(msg);
-                if(str!=""){
-                 $("#chatmsg").append(str);  
-                }
-            }
-            $("#mywords").val('');
-        });
-        
-        /**
-         * 监听自定义消息
-         */
-        window.chat.onCustomMsg(function(msg){
-             msg = JSON.parse(msg);
-             try{
-             	var e="";
-                if(msg.type ==10 ){//聊天
-                    e+=liaotian(msg);
-                }else if(msg.type == 11){ //礼物
-                    e+=liveGiftList(msg);
-                     //在礼物区域显示
-                	createGiftList(msg.message);
-                }else if(msg.type == 12){ // 开始直播啦
-                
-                	
-                }if(msg.type == 13){ //直播结束了  
-                
-                } else if (msg.type == 14) { // 退出直播间，但是没有结束直播
-
-				} else if (msg.type == 15) { // 继续直播
-
-				} else if (msg.type == 16) { // 回放生成成功
-
-				} else if (msg.type == 17) { // 回放生成失败
-
-				}
-                if (e != "") {
-					$("#chatmsg").append(e);
-				}
-             }catch(error){
-               console.error(error);
-             }
-        })
-        
-        /**
-         * 某某进入直播间
-         */
         window.chat.join(function(msg){
             viewJoinleaveRoomInfo(msg,"join");
         })
-        
-        /**
-         * 某某离开直播间
-         */
         window.chat.leave(function(msg){
             viewJoinleaveRoomInfo(msg,"leave");
         })
-        
      });	
-    
      window.Vhall.config({
           appId :vhallObj.appId,//应用 ID ,必填
           accountId :vhallObj.accountId,//第三方用户唯一标识,必填
           token:vhallObj.token//token必填
      });
-     
     },1000);	
       
     /**
@@ -228,8 +138,6 @@ function elsBind(){
         }
     });
 }
-//初始化    
-elsBind();
 
 /**
  * 获取消息列表
