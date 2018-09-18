4279d6935ad3fbd015de929df1c6daf9ae24310e
yuruixin <yuruixin@ixincheng.com>
2018-09-18 16:12:31 +0800

    [feat]库存完成、地址管理

--- wechat/src/main/java/com/xczh/consumer/market/controller/shop/ShopOrderController.java
+++ wechat/src/main/java/com/xczh/consumer/market/controller/shop/ShopOrderController.java
@@ -3,12 +3,16 @@ package com.xczh.consumer.market.controller.shop;
 import com.xczh.consumer.market.auth.Account;
 import com.xczh.consumer.market.utils.ResponseObject;
 import net.shopxx.merge.service.OrderOperService;
+import net.shopxx.merge.vo.ReceiverVO;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -30,8 +34,8 @@ public class ShopOrderController {
     }
 
     @RequestMapping("checkout")
-    public ResponseObject checkout(@Account String accountId, Long skuId, Integer quantity) {
-        Map<String, Object> map = orderOperService.checkout(skuId, quantity, accountId);
+    public ResponseObject checkout(@Account String accountId, Long skuId, Integer quantity,String cartItemIds) {
+        Map<String, Object> map = orderOperService.checkout(skuId, quantity, cartItemIds, accountId);
         return ResponseObject.newSuccessResponseObject(map);
     }
 
@@ -51,12 +55,44 @@ public class ShopOrderController {
      * @return
      */
     @RequestMapping("/order/create")
-    public ResponseObject createOrder(@Account String accountId,Long skuId, Integer quantity, String cartTag, Long receiverId, Long paymentMethodId, Long shippingMethodId,
-                                         String code, String invoiceTitle, String invoiceTaxNumber, BigDecimal balance, String memo){
-        Map<String, Object> map = orderOperService.create(skuId, quantity, cartTag, receiverId, paymentMethodId, shippingMethodId, code, invoiceTitle, invoiceTaxNumber, balance, memo, accountId);
+    public ResponseObject createOrder(@Account String accountId,String cartItemIds,Long skuId, Integer quantity, String cartTag, Long receiverId, Long paymentMethodId, Long shippingMethodId,
+                                         String code, String invoiceTitle, String invoiceTaxNumber, BigDecimal balance, String memo ){
+        Map<String, Object> map = orderOperService.create(cartItemIds,skuId, quantity, cartTag, receiverId, paymentMethodId, shippingMethodId, code, invoiceTitle, invoiceTaxNumber, balance, memo, accountId);
         return ResponseObject.newSuccessResponseObject(map);
     }
 
+    @RequestMapping(value = "/receiver/add")
+    public ResponseObject addReceiver(@Account String accountId, @RequestParam String consignee, @RequestParam String address
+                                    , @RequestParam String zipCode, @RequestParam String phone
+                                    , @RequestParam Boolean isDefault, @RequestParam Long areaId){
+        orderOperService.addReceiver(consignee,address,zipCode,phone,isDefault,areaId,accountId);
+        return ResponseObject.newSuccessResponseObject(null);
+    }
 
+    @RequestMapping("/receiver/list")
+    public ResponseObject receiverlist(@Account String accountId){
+        List<ReceiverVO> receiverlist = orderOperService.receiverlist(accountId);
+        return ResponseObject.newSuccessResponseObject(receiverlist);
+    }
+
+    @RequestMapping(value = "/receiver",method = RequestMethod.GET)
+    public ResponseObject receiver(@Account String accountId,@RequestParam Long receiverId){
+        ReceiverVO receiver = orderOperService.findReceiverById(receiverId, accountId);
+        return ResponseObject.newSuccessResponseObject(receiver);
+    }
+
+    @RequestMapping("/receiver/update")
+    public ResponseObject updateReceiver(@Account String accountId, @RequestParam String consignee, @RequestParam String address
+            , @RequestParam String zipCode, @RequestParam String phone
+            , @RequestParam Boolean isDefault, @RequestParam Long areaId, @RequestParam Long receiverId){
+        orderOperService.updateReceiver(receiverId,consignee,address,zipCode,phone,isDefault,areaId,accountId);
+        return ResponseObject.newSuccessResponseObject(null);
+    }
+
+    @RequestMapping("/receiver/delete")
+    public ResponseObject deleteReceiver(@Account String accountId, @RequestParam Long receiverId){
+        orderOperService.deleteReceiver(receiverId,accountId);
+        return ResponseObject.newSuccessResponseObject(null);
+    }
 
 }
