9e63d47f1c16bd969c00f069af4716d6346983a8
wangxingchuan <695014849@qq.com>
2018-09-18 23:46:25 +0800

    退款

---
+++ wechat/src/main/webapp/xcview/js/shop/shop-refund-only.js
@@ -0,0 +1,32 @@
+$(function(){
+	$('.save-pic ul').on('click',"p",function(){
+		$(this).parent().remove();
+		isPiclength();
+	});
+//	开启弹窗
+	$(".goods-select").click(function(){
+		$(".gray-bgcolor").removeClass("hide");
+		$(".goos-status-modal").animate({"bottom":"0"},300)
+	});
+	$(".return-goods-reason").click(function(){
+		$(".gray-bgcolor").removeClass("hide");
+		$(".return-goods-modal").animate({"bottom":"0"},300)
+	});
+//	关闭弹窗
+	$(".gray-bgcolor, .goos-status-top img, .return-goods-top img").click(function(){
+		$(".gray-bgcolor").addClass("hide");
+		$(".comment-modal").stop().animate({"bottom":"-6rem"},200)
+	});
+//	选中货物状态
+	$(".goos-status-select ul li span").click(function(){
+		$(".goos-status-select ul li span").html("")
+		$(this).html('<img src="/xcview/images/select-status.png" alt="选中" />')
+	});
+//	选中退货原因
+	$(".return-goods-select ul li span").click(function(){
+		$(".return-goods-select ul li span").html("")
+		$(this).html('<img src="/xcview/images/select-status.png" alt="选中" />')
+	});	
+	
+	
+})
