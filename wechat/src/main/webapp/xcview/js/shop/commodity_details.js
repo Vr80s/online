92ff18c0615ec3c274c163fa31920cc2905cf6bc
haozhognping <haozhongping@ixincheng.com>
2018-09-27 23:20:37 +0800

    测试-点击li商品规格

--- wechat/src/main/webapp/xcview/js/shop/commodity_details.js
+++ wechat/src/main/webapp/xcview/js/shop/commodity_details.js
@@ -52,10 +52,57 @@ requestGetService("/xczh/shop/goods/details",{
         $(".message").html(template('message', {items: obj}));
         // 选择规格
         $(".specifications").html(template('specifications', {item: obj.specificationItemvs}));
+        // $(".specificationsss").html(template('specificationsss', {item: obj.specificationItemvs}));
         
+
+
+        // 点击规格
+        /*var aBtn=$('.specifications_ul .casing');
+        for(i=0;i<aBtn.length;i++){
+            $(aBtn[i]).click(function(){
+                var specification = document.querySelector(".specification");  //规格
+                specification.style.display="none";
+                // 判断显示已选择
+                if ($(".include").hasClass("public")) {
+                    $(".kind").show();
+                    $(".specification").show();
+                    $(".choice").html("已选择");
+                };
+                for(i=0;i<aBtn.length;i++){
+                    $(aBtn[i]).siblings().removeClass('public');
+                }
+                $(this).addClass('public');
+                // var detaId = $(this).attr("data-id");
+                alert(detaId);
+                var publicHtml = $(this).html();
+                $(".specification").html(publicHtml);
+            })
+        };*/
+
+        $('.specifications_ul .casing').click(function(){
+             // 判断显示已选择
+                if ($(".include").hasClass("public")) {
+                    $(".kind").show();
+                    $(".specification").show();
+                    $(".choice").html("已选择");
+                };
+                $(this).addClass('public');
+                $(this).siblings().removeClass('public');
+                var publicHtml = $(this).html();
+                $(".specification").html(publicHtml);
+
+                /*var publicHtml = $(this).html();
+                var detaId = $(this).attr("data-ids");// 一级id
+                $(".fl1_"+detaId).html(publicHtml);*/
+
+        });
+
+
 
     }
 });
 
-
+/*$(".specifications_ul .include").click(function(){
+        alert(11111);
+    });*/
 
