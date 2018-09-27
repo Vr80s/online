90d4d8bffdfec248fea0b1d5985303d44fe75eb7
haozhognping <haozhongping@ixincheng.com>
2018-09-27 10:18:32 +0800

    修改商品详情轮播区域

--- wechat/src/main/webapp/xcview/js/shop/commodity_details.js
+++ wechat/src/main/webapp/xcview/js/shop/commodity_details.js
@@ -6,11 +6,16 @@ requestGetService("/xczh/shop/goods/details",{
 },function (data) {
     if (data.success == true) {
         var obj = data.resultObject;
+        // 详情
         $(".list_details").html(template('list_details', {items: obj}));
         // $(".swiper-wrapper").html(template('top_details', {items: obj}));
         // $(".banner").html(template('top_details', {items: obj}));
+        // 轮播图
         $(".banner").html(template('top_details', {items: obj.productImages}));
-        
+        var swiper = new Swiper('.swiper-container', {
+            pagination: '.swiper-pagination',
+            paginationType: 'fraction'
+        });
         // 判断商品介绍为空
         if (obj.caption == null) {
             $(".referral").hide();
@@ -22,6 +27,7 @@ requestGetService("/xczh/shop/goods/details",{
             pagination: '.swiper-pagination',
             paginationType: 'fraction'
         });*/
+        
 
 
 
