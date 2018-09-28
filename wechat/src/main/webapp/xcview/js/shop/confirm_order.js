fd9b6a58e5cfed54369e87e8c2e943cf571465e5
yuruixin <yuruixin@ixincheng.com>
2018-09-28 09:03:17 +0800

    [feat]下单接口相关

--- wechat/src/main/webapp/xcview/js/shop/confirm_order.js
+++ wechat/src/main/webapp/xcview/js/shop/confirm_order.js
@@ -34,8 +34,8 @@ $(function() {
         var memo = "拒收到付";
         requestService("/xczh/shop/order/create","skuId="+skuId+"&quantity="+quantity+"&shippingMethodId="+shippingMethodId+"&receiverId="+receiverId+"&memo="+memo,function(data){
             if(data.success){
-                var orderSns = data.resultObject.orderSns;
-                window.location="http://localhost:8282/order/payment?orderSns="+orderSns;
+                var orderSns = data.resultObject.orderSns.join(',');
+                window.location="http://cs.shop.xczhihui.com/order/payment?orderSns="+orderSns;
             }else{
                 alert(data.errorMessage);
             }
