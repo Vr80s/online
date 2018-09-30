4198642de599f556bb3c670b059c6bd5ac9eb1e6
MSI\yangxuan <453099975@qq.com>
2018-09-30 08:59:09 +0800

    修改错误的对应bean类

--- online-web/src/main/java/com/xczhihui/bxg/online/web/controller/shop/ShopGoodsController.java
+++ online-web/src/main/java/com/xczhihui/bxg/online/web/controller/shop/ShopGoodsController.java
@@ -1,30 +1,15 @@
 package com.xczhihui.bxg.online.web.controller.shop;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.propertyeditors.CustomDateEditor;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.xczhihui.bxg.online.web.base.utils.UserLoginUtil;
-import com.xczhihui.common.support.domain.BxgUser;
 import com.xczhihui.common.util.bean.ResponseObject;
 
-import net.shopxx.merge.enums.OrderType;
-import net.shopxx.merge.enums.Status;
-import net.shopxx.merge.enums.Type;
-import net.shopxx.merge.enums.UsersType;
 import net.shopxx.merge.service.GoodsService;
-import net.shopxx.merge.service.OrderOperService;
-import net.shopxx.merge.vo.OrderPageParams;
 
 /**
  * 
