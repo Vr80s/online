abcffbb24f4ba208b64008f6471ff3b50981875c
wangxingchuan <695014849@qq.com>
2018-09-30 15:03:34 +0800

    商品预览

--- online-web/src/main/webapp/web/js/anchors_preview.js
+++ online-web/src/main/webapp/web/js/anchors_preview.js
@@ -9,9 +9,9 @@ $(function(){
     return null;
 	};
 
-	var productId=getQueryString("productId");
+	var productId=getQueryString("productId"); //10152
 	 RequestService("/xczh/shop/good/detail", "GET", {
-	 	"productId":10152
+	 	"productId":productId
 	 } , function (data) {
      	if (data.success==true) {
  			var obj=data.resultObject;
@@ -19,32 +19,37 @@ $(function(){
 			var anchorsData=obj.posts;
 			var evaluateData=obj.reviewvs;
 	//  轮播图
-			if(bannerData.length==0){
-				$(".banner-wrap").addClass("hide");
-			}else{
-				var str="";
-				for(var i=0; i<bannerData.length;i++){
-					str+='<div class="item">'+
-				    		'<img src="'+bannerData[i].source+'" alt="广告图">'+
-				  		'</div>'
-				}
-				$(".banner-carousel").html(str)
-			}
+//			if(bannerData.length==0){
+//				$(".banner-wrap").addClass("hide");
+//			}else{
+//				var str="";
+//				for(var i=0; i<bannerData.length;i++){
+//					str+='<div class="item">'+
+//				    		'<img src="'+bannerData[i].source+'" alt="广告图">'+
+//				  		'</div>'
+//				}
+//				$(".banner-carousel").html(str)
+//			}
 //		商品名/价格/已售
 			$(".shop-detail-wrap").html(template("detail-template",{item:obj}))
 //		医师推荐
-			if(anchorsData.length==0){
+			if(anchorsData.length== 0 || anchorsData== ""){
 				$(".physician-main").addClass("hide");
 			}else{
 				$("#physician-main").html(template("physician-template",{item:obj.posts[0]}))
 			}
 //		评价
-			if(evaluateData.length==0){
+			if( evaluateData== null || evaluateData==""){
 				$(".ware-evaluate").addClass("hide");
 			}else{
 				$(".ware-evaluate").html(template("evaluate-template",{item:obj.reviewvs[0]}))
 			}
-
+//		商品详情
+			if(obj.introduction==null || obj.introduction == ""){	
+				$(".ware-detail").addClass("hide");
+			}else{
+				$(".ware-detail-img").html(obj.introduction);				
+			}
 
 
 
