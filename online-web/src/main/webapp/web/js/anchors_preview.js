bdd7c76fb1c38bccaff57812f44580d8e55b0016
wangxingchuan <695014849@qq.com>
2018-10-08 14:31:19 +0800

    商品预览轮播，二维码

--- online-web/src/main/webapp/web/js/anchors_preview.js
+++ online-web/src/main/webapp/web/js/anchors_preview.js
@@ -19,17 +19,34 @@ $(function(){
 			var anchorsData=obj.posts;
 			var evaluateData=obj.reviewvs;
 	//  轮播图
-//			if(bannerData.length==0){
-//				$(".banner-wrap").addClass("hide");
-//			}else{
-//				var str="";
-//				for(var i=0; i<bannerData.length;i++){
-//					str+='<div class="item">'+
-//				    		'<img src="'+bannerData[i].source+'" alt="广告图">'+
-//				  		'</div>'
-//				}
-//				$(".banner-carousel").html(str)
-//			}
+	 		
+			if(bannerData.length==0){
+				$(".banner-wrap").addClass("hide");
+			}else{
+//				轮播图个数
+				for(var i=0;i<bannerData.length; i++){
+	                if(i == 0){
+	                    $(".banner-indicators").append("<li data-target='#carousel-example-generic' data-slide-to='0'  class='active'></li>")
+	                }else{
+	                    $(".banner-indicators").append("<li data-target='#carousel-example-generic' data-slide-to='"+i+"'></li>")
+	                }
+	            }
+//				轮播图张数
+				for(var i=0; i<bannerData.length;i++){
+				  	if(i == 0) {
+                        $(".banner-carousel").append('<div class="item active">'+
+			    		'<img src="'+bannerData[i].source+'" alt="广告图">'+
+			  			'</div>'
+			  			);
+                    } else {
+                        $(".banner-carousel").append('<div class="item">'+
+			    		'<img src="'+bannerData[i].source+'" alt="广告图">'+
+			  			'</div>'
+			  			);
+                   }
+
+				}
+			}
 //		商品名/价格/已售
 			$(".shop-detail-wrap").html(template("detail-template",{item:obj}))
 //		医师推荐
@@ -50,18 +67,20 @@ $(function(){
 			}else{
 				$(".ware-detail-img").html(obj.introduction);				
 			}
-
-
-
-
-
-
-
-
-
-
      	} else{
      		showTip("errorMessage")
      	}
 	 })
+	 
+	 
+//	 生成二维码
+	$(".wechat-data").qrcode({
+		render : "canvas",    //设置渲染方式，有table和canvas，使用canvas方式渲染性能相对来说比较好
+		text : "http://www.baidu.com",    //扫描了二维码后的内容显示,在这里也可以直接填一个网址，扫描二维码后
+		width : "158",               //二维码的宽度
+		height : "158",              //二维码的高度
+		background : "#ffffff",       //二维码的后景色
+		foreground : "#000000",        //二维码的前景色
+		src: '/web/images/yrx.png'             //二维码中间的图片
+	});
 })
