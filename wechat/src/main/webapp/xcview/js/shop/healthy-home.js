450d0b2e4cc72c3de60443155c52aecd74292859
wangxingchuan <695014849@qq.com>
2018-10-12 23:49:09 +0800

    mui

--- wechat/src/main/webapp/xcview/js/shop/healthy-home.js
+++ wechat/src/main/webapp/xcview/js/shop/healthy-home.js
@@ -3,4 +3,13 @@ $(function(){
 		$(".sex-ul li img").removeClass("active");
 		$(this).find("img").addClass("active");
 	})
+document.getElementById('liMONTH').addEventListener('tap',function () {
+    var datePicker=new mui.DtPicker({
+	    type:'month',//若只显示年月日，类型为date，
+	});
+	datePicker.show(function(selectItems){
+	    h("#datMONTH").val(selectItems);
+    });
+})
+
 })
