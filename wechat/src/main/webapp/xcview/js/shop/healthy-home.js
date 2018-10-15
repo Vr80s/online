4a36a4c0a25fc9f7be7252af65d1989cc01149d3
wangxingchuan <695014849@qq.com>
2018-10-15 09:56:15 +0800

    健康诊疗，页面一完成

--- wechat/src/main/webapp/xcview/js/shop/healthy-home.js
+++ wechat/src/main/webapp/xcview/js/shop/healthy-home.js
@@ -4,11 +4,16 @@ $(function(){
 		$(this).find("img").addClass("active");
 	})
 document.getElementById('liMONTH').addEventListener('tap',function () {
-    var datePicker=new mui.DtPicker({
+   var minDate=new Date();
+   		minDate.setFullYear(1900,01,01)
+   var datePicker=new mui.DtPicker({
 	    type:'month',//若只显示年月日，类型为date，
+	    minDate:minDate,
+ 		beginDate: new Date(1900, 04, 25),//设置开始日期 
+    	endDate: new Date()//设置结束日期 
 	});
 	datePicker.show(function(selectItems){
-	    h("#datMONTH").val(selectItems);
+	    $("#datMONTH").val(selectItems);
     });
 })
 
