51afbcc04071dff066750b3e18cafac776c57384
wangyishuai <15210815880@163.com>
2018-10-16 19:02:10 +0800

    修改订单详情接口

--- shop/src/main/java/net/shopxx/merge/service/impl/OrderOperServiceImpl.java
+++ shop/src/main/java/net/shopxx/merge/service/impl/OrderOperServiceImpl.java
@@ -489,6 +489,7 @@ public class OrderOperServiceImpl implements OrderOperService {
 				orderItemVO.setId(orderItem.getId());
 				//获取库存
 				SkuVO sku = new SkuVO();
+				ProductVO product = new ProductVO();
 				if(orderItem.getSku() != null){
 					List<String> specification = orderItem.getSku().getSpecifications();
 					if(specification.size()>0){
@@ -496,9 +497,11 @@ public class OrderOperServiceImpl implements OrderOperService {
 						sku.setSpecifications(citiesCommaSeparated);
 					}
 					BeanUtils.copyProperties(orderItem.getSku(),sku);
+					product.setId(orderItem.getSku().getProduct().getId());
 					sku.setId(orderItem.getSku().getId());
 				}
 				orderItemVO.setSku(sku);
+				orderItemVO.getSku().setProduct(product);
 				orderItemVOList.add(orderItemVO);
 			}
 			o.setOrderItems(orderItemVOList);
