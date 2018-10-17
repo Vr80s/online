e54673073d4def9b9ddc9177f2efafaa36d90a77
MSI\yangxuan <453099975@qq.com>
2018-10-17 09:49:45 +0800

    统一下详情规格选取

--- wechat/src/main/webapp/xcview/js/shop/commodity_details.js
+++ wechat/src/main/webapp/xcview/js/shop/commodity_details.js
@@ -29,31 +29,39 @@ requestGetService("/xczh/shop/goods/details",{
         var obj = data.resultObject;
         // 详情
         $(".list_details").html(template('list_details', {items: obj}));
-        var v = $(".ruling_price span").html();
-		if(/^\d+$/.test(v)){
-		v = v + ".00";
-		}else if(/^(\d+\.)(\d+)$/.test(v)){
-			var i = RegExp.$1;
-			var t = RegExp.$2;
-			if(t.length == 1)
-			v = v + "0";
-			else if(t.length > 2)
-			v = i + t.substring(0,2);
-		}
-		$(".ruling_price span").html(v);
-		
-		var v = $(".original_price span").html();
-		if(/^\d+$/.test(v)){
-		j = j + ".00";
-		}else if(/^(\d+\.)(\d+)$/.test(j)){
-			var i = RegExp.$1;
-			var t = RegExp.$2;
-			if(t.length == 1)
-			j = j + "0";
-			else if(t.length > 2)
-			j = i + t.substring(0,2);
-		}
-		$(".original_price span").html(j);
+        
+        try{
+        
+        	var v = $(".ruling_price span").html();
+			if(/^\d+$/.test(v)){
+			v = v + ".00";
+			}else if(/^(\d+\.)(\d+)$/.test(v)){
+				var i = RegExp.$1;
+				var t = RegExp.$2;
+				if(t.length == 1)
+				v = v + "0";
+				else if(t.length > 2)
+				v = i + t.substring(0,2);
+			}
+			$(".ruling_price span").html(v);
+			
+			
+			var v = $(".original_price span").html();
+			if(/^\d+$/.test(v)){
+			j = j + ".00";
+			}else if(/^(\d+\.)(\d+)$/.test(j)){
+				var i = RegExp.$1;
+				var t = RegExp.$2;
+				if(t.length == 1)
+				j = j + "0";
+				else if(t.length > 2)
+				j = i + t.substring(0,2);
+			}
+			$(".original_price span").html(j);
+        	
+        }catch(e){
+           console.error(e);
+        }
 
         // $(".swiper-wrapper").html(template('top_details', {items: obj}));
         // $(".banner").html(template('top_details', {items: obj}));
@@ -118,149 +126,7 @@ requestGetService("/xczh/shop/goods/details",{
         
         $(".category").html(template('category', {item: obj.specificationItemvs}));
         
-
-        var skus = obj.skuVOs;
-        var specificationIds = [];
-        var defaultSkus = {};
-        
-        for (var i = 0; i < skus.length; i++) {
-        	if(skus[i].isDefault){
-        		specificationIds = skus[i].specificationValueIds;
-        		defaultSkus = skus[i];
-        	}
-        	if(skus[i].specificationValueIds!=null &&
-        		skus[i].specificationValueIds.length>0){
-        			
-        	   skus[i].specificationIdsStr = skus[i].specificationValueIds.sort().join(',');
-        	}
-        }
-        
-          /**
-         * 显示库存是否充足
-         * 	当前价格 
-         */
-        if(defaultSkus!=null){
-        	
-        	if(defaultSkus.isOutOfStock){
-        		 $(".information .repertory").html("该商品库存不足");
-        		 $(".shopping_trolley_center .determine").css("background","#aaaaaa");
-        	}else{
-        		 $(".information .repertory").html("库存"+defaultSkus.availableStock+"件");
-        		 $(".shopping_trolley_center .determine").css("background","#F97215");
-        	}
-         	$(".information .price").html(defaultSkus.price);
-         	
-         	var j = $(".information .price").html();
-			if(/^\d+$/.test(j)){
-			j = j + ".00";
-			}else if(/^(\d+\.)(\d+)$/.test(j)){
-				var i = RegExp.$1;
-				var t = RegExp.$2;
-				if(t.length == 1)
-				j = j + "0";
-				else if(t.length > 2)
-				j = i + t.substring(0,2);
-			}
-			//alert(v);
-			$(".information .price").html(j);
-         	
-         	
-       	    currentSku = defaultSkus;
-        }
-        
-		//默认选中
-//        $(".specification").each(function(index,obj){
-//        	 var dataId = $(obj).attr("data-id");
-//        	 for (var i = 0; i < specificationIds.length; i++) {
-//	        	 if(dataId == specificationIds[i]){
-//	        	 	$(obj).removeClass("hide");
-//	        	 }
-//        	 }
-//        })
-        
-//        $('.specifications_ul .casing').each(function(index,obj){
-//        	 var dataId = $(obj).attr("data-id");
-//        	 for (var i = 0; i < specificationIds.length; i++) {
-//	        	 if(dataId == specificationIds[i]){
-//	        	 	$(obj).addClass("public");
-//	        	 }
-//        	 }
-//        })
-        
-//     	 点击规格li
-         $('.specifications_ul .casing').click(function(){
-         	
-         	currentSku =null;
-         	
-         	// 判断显示已选择
-            if ($(".include").hasClass("public")) {
-                $(".kind").show();
-                $(".specification").show();
-            };
-            $(this).addClass('public');
-            $(this).siblings().removeClass('public');
-            
-            $(".specification").addClass("hide");
-            $(".specification").removeClass("showfalg");
-            
-            var lalala = [];
-            $('.specifications_ul .casing[class*="public"]').each(function(index,obj){
-            	 
-            	 var dataId = $(obj).attr("data-id");
-            	 $(".specification").each(function(index,objs){
-            	 	 var dataIds = $(objs).attr("data-id");
-            	 	 var dataName = $(objs).attr("title");
-		        	 if(dataId == dataIds){
-		        	 	$(objs).addClass("showfalg");
-		        	 	lalala.push(dataIds);
-		        	 	 $(".specification_name").each(function(index,obj){
-		        	 		var title = $(obj).attr("title");
-		        	 		if(dataName == title){
-		        	 			$(obj).addClass("hide");
-		        	 		}
-		        	 	})
-		        	 }
-		        })
-            })
-            
-		    var specificationNames = $(".specification_name[class*='hide']");
-    	 	if(specificationNames!=null
-    	 		    && specificationNames.length == specificationsResutl.length){
-    	 			
-    	 		 $(".shopping_trolley_center .determine").css("background","#F97215");   	
-    	 		 $(".showfalg").removeClass("hide");
-    	 		 $(".choice").html("已选择");
-    	 		 
-    	 		 
-	 		    if(lalala!=null){
-	             	lalala.sort();
-	            }
-	            
-	            for (var i = 0; i < skus.length; i++) {
-	            	if(skus[i].specificationIdsStr == lalala.join(",")){
-	            		currentSku = skus[i];
-	            	}
-	            }
-	            
-	            if(currentSku!=null){
-	            	if(currentSku.isOutOfStock){
-		        		 $(".information .repertory").html("该商品库存不足");
-		        		 $(".shopping_trolley_center .determine").css("background","#aaaaaa");
-		        	}else{
-		        		 $(".information .repertory").html("库存"+currentSku.availableStock+"件");
-		        		 $(".shopping_trolley_center .determine").css("background","#F97215");
-		        	}
-		         	$(".information .price").html(currentSku.price);
-	        	}else{
-	        	 	$(".information .repertory").html("该商品库存不足");
-		            $(".shopping_trolley_center .determine").css("background","#aaaaaa");
-	        	}
-    	 	}else{
-    	 		$(".shopping_trolley_center .determine").css("background","#aaaaaa");
-    	 	}
-            
-        });
-        
+        specificationChoose(obj);
     }
 });
 
